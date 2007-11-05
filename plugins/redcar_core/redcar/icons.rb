50fd567d268ae173563775ed3aee472ff05f9084
Daniel Lucraft <dan@fluentradical.com>
2007-11-05 17:56:44 +0000

    Icons now take the size from the GNOME settings.

--- plugins/redcar_core/redcar/icons.rb
+++ plugins/redcar_core/redcar/icons.rb
@@ -2,7 +2,10 @@ module Redcar
   class Icon
     def self.get(name)
       icon = eval("Gtk::Stock::"+name.to_s.upcase)
-#       Gtk::Image.new(icon, Gtk::IconSize::MENU)
+      
+    end
+    def self.get_image(name, size=Gtk::IconSize::DND)
+      Gtk::Image.new(self.get(name), size)
     end
   end
 end
