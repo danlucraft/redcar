536dbf94ace4531105a1bc3b1571f178fd1b2cf6
Daniel Lucraft <dan@fluentradical.com>
2008-01-07 07:13:40 +0000

    Added startup scripting

---
+++ .Redcar/startup.rb
@@ -0,0 +1,12 @@
+
+puts "(startup.rb)"
+
+#puts "performance test"
+#require 'ruby-prof'
+
+#new_tab = Redcar.new_tab
+#new_tab.filename = "/home/dan/projects/redcar/freebase2/lib/freebase/readers.rb"  
+#RubyProf.start
+#new_tab.load  
+#result = RubyProf.stop
+
--- plugins/scripting/scripting.rb
+++ plugins/scripting/scripting.rb
@@ -1,5 +1,5 @@
 
-require 'gnomevfs'
+#require 'gnomevfs'
 
 module Redcar
   module Plugins
@@ -9,7 +9,22 @@ module Redcar
       extend Redcar::MenuBuilder
       extend Redcar::CommandBuilder
       
+      STARTUP_SCRIPT = File.expand_path("~/.Redcar/startup.rb")
+      
+      def self.run_startup_script
+        if File.exists?(STARTUP_SCRIPT)
+          require STARTUP_SCRIPT
+        else
+          puts "(no startup script)"
+        end
+      end
+      
       def self.load(plugin)
+        $BUS["/system/state/all_plugins_loaded"].subscribe do |event, slot|
+          if $BUS["/system/state/all_plugins_loaded"].data.to_bool
+            run_startup_script
+          end
+        end
         plugin.transition(FreeBASE::LOADED)
       end
     end
