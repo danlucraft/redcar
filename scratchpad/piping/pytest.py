d75ac9083069d18d0518fca9436c694d62c28134
Daniel Lucraft <dan@fluentradical.com>
2009-02-18 22:09:53 +0000

    More fiddling with process control

--- scratchpad/piping/pytest.py
+++ scratchpad/piping/pytest.py
@@ -1,6 +1,13 @@
+
 import sys, os, re, traceback
+
 pattern = re.compile("and")
+<<<<<<< HEAD:scratchpad/piping/pytest.py
 mate = "/home/dan/projects/redcar/bin/redcar -d --log"
+=======
+
+mate = "/home/dan/Projects/redcar/bin/redcar"
+>>>>>>> More fiddling with process control:scratchpad/pytest.py
 pb = os.popen(mate, "w")
 for line in sys.stdin:
   if pattern.search(line):
