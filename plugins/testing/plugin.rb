dae2c244bf8a4db51afe9f95b4fd65352d5d9e44
Daniel Lucraft <dan@fluentradical.com>
2009-02-25 08:18:33 +0000

    oops, fixed specs

--- plugins/testing/plugin.rb
+++ plugins/testing/plugin.rb
@@ -8,5 +8,6 @@ end
 load File.dirname(__FILE__) + "/lib/testing.rb"
 load File.dirname(__FILE__) + "/lib/runners/rspec/formatters.rb"
 load File.dirname(__FILE__) + "/lib/runners/internal_rspec_runner.rb"
+load File.dirname(__FILE__) + "/lib/runners/internal_cucumber_runner.rb"
 load File.dirname(__FILE__) + "/tabs/test_view_tab.rb"
 
