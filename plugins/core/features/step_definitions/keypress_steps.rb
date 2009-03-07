f285904f727af58b8237893490e672190e7f5327
Daniel Lucraft <dan@fluentradical.com>
2009-03-07 10:23:34 +0000

    Keypress step now raises press and release

--- plugins/core/features/step_definitions/keypress_steps.rb
+++ plugins/core/features/step_definitions/keypress_steps.rb
@@ -7,6 +7,34 @@ def clean_letter(letter)
   end
 end
 
+def make_event_key(key, type)
+  # puts "pressing: #{key}"
+  bits = key.split("+")
+  ctrl = (bits.include?("Ctrl")  ? true : false)
+  alt  = (bits.include?("Alt")   ? true : false)
+  supr = (bits.include?("Super") ? true : false)
+  letter = clean_letter(bits.last)
+  shift = (bits.include?("Shift") && (letter =~ /^[[:alpha:]]$/ or letter.length > 1)? true : false)
+  case type
+  when :release
+    new_event_key = Gdk::EventKey.new(Gdk::Event::KEY_RELEASE)
+  when :press
+    new_event_key = Gdk::EventKey.new(Gdk::Event::KEY_PRESS)
+  end
+  
+  new_mod_mask = 0
+  new_mod_mask |= Gdk::Window::ModifierType::SHIFT_MASK if shift
+  new_mod_mask |= Gdk::Window::ModifierType::CONTROL_MASK if ctrl
+  new_mod_mask |= Gdk::Window::ModifierType::MOD1_MASK if alt
+  new_mod_mask |= Gdk::Window::ModifierType::SUPER_MASK if supr
+  new_mod_mask = Gdk::Window::ModifierType.new(new_mod_mask)
+  new_event_key.state = new_mod_mask
+  new_event_key.keyval = Gdk::Keyval.from_name(letter)
+  
+  new_event_key.window = Redcar.win.window
+  new_event_key
+end
+
 def inspect_event_key(gdk_event_key)
   kv = gdk_event_key.keyval
   ks = gdk_event_key.state - Gdk::Window::MOD2_MASK
@@ -26,27 +54,8 @@ When /^I wait for all GUI events to be processed$/ do
 end
 
 When /^I press "(.*)"$/ do |key|
-  # puts "pressing: #{key}"
-  bits = key.split("+")
-  ctrl = (bits.include?("Ctrl")  ? true : false)
-  alt  = (bits.include?("Alt")   ? true : false)
-  supr = (bits.include?("Super") ? true : false)
-  letter = clean_letter(bits.last)
-  shift = (bits.include?("Shift") && (letter =~ /^[[:alpha:]]$/ or letter.length > 1)? true : false)
-  new_event_key = Gdk::EventKey.new(Gdk::Event::KEY_RELEASE)
-  
-  new_mod_mask = 0
-  new_mod_mask |= Gdk::Window::ModifierType::SHIFT_MASK if shift
-  new_mod_mask |= Gdk::Window::ModifierType::CONTROL_MASK if ctrl
-  new_mod_mask |= Gdk::Window::ModifierType::MOD1_MASK if alt
-  new_mod_mask |= Gdk::Window::ModifierType::SUPER_MASK if supr
-  new_mod_mask = Gdk::Window::ModifierType.new(new_mod_mask)
-  new_event_key.state = new_mod_mask
-  new_event_key.keyval = Gdk::Keyval.from_name(letter)
-  
-  inspect_event_key(new_event_key)
-  new_event_key.window = Redcar.win.window
-  new_event_key.put
+  make_event_key(key, :press).put
+  make_event_key(key, :release).put
 end
 
 
