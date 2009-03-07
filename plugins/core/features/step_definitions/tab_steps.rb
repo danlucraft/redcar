497bff905dce9e2d37186181aa723b4bd73bfa98
Daniel Lucraft <dan@fluentradical.com>
2009-03-07 10:43:27 +0000

    Improved "tabs open" step

--- plugins/core/features/step_definitions/tab_steps.rb
+++ plugins/core/features/step_definitions/tab_steps.rb
@@ -1,4 +1,8 @@
 
-Then /^there should be (\d) tabs? open$/ do |num|
-  Redcar.win.tabs.length.should == num.to_i
+Then /^there should be (\d) (?:(\w+)s?|tabs?) open$/ do |num, type|
+  if type
+    Redcar.win.collect_tabs(Redcar.const_get(type)).length.should == num.to_i
+  else
+    Redcar.win.tabs.length.should == num.to_i
+  end
 end
--- plugins/core/features/tab.feature
+++ plugins/core/features/tab.feature
@@ -2,4 +2,4 @@ Feature: Opening and closing tabs
   
 Scenario: Open an EditTab
   When I press "Super+N"
-  Then there should be 1 tab open
+  Then there should be 1 EditTab open
