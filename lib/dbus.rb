2ee855b867027af3b0696661b33bcc708d44e9f8
Daniel Lucraft <dan@fluentradical.com>
2009-03-19 08:13:34 +0000

    Now a single-instance application

--- lib/dbus.rb
+++ lib/dbus.rb
@@ -12,13 +12,22 @@ module Redcar
       service      = @bus.request_service(DBUS_REDCAR_SERVICE)
       exported_obj = new(DBUS_REDCAR_PATH)
       
-      puts "Exporting DBus object"
       service.export(exported_obj)      
     end
     
     dbus_interface DBUS_REDCAR_INTERFACE do
+      dbus_method :focus do
+        Redcar.win.window.focus Gdk::Event::CURRENT_TIME
+      end
+      
       dbus_method :open, "in path:s" do |path|
-        puts "open(#{path.inspect})" 
+        # TODO: fix this hardcoded reference
+        OpenTab.new(path).do
+      end
+      
+      dbus_method :new_tab, "in contents:s" do |contents|
+        tab = NewTab.new.do
+        tab.buffer.text = contents
       end
     end
     
@@ -26,13 +35,20 @@ module Redcar
     def self.try_export_service
       Redcar::DBus.export_service
     rescue ::DBus::Connection::NameRequestError
-      puts "Redcar is already running, trying to send message"
       bus     = ::DBus::SessionBus.instance
       service = bus.service(DBUS_REDCAR_SERVICE)
       object  = service.object(DBUS_REDCAR_PATH)
       object.introspect
       object.default_iface = DBUS_REDCAR_INTERFACE
-      object.open("helloworld")
+      ARGV.each do |arg|
+        if File.exist?(arg)
+          object.open(File.expand_path(arg))
+        end
+      end
+      if $stdin_contents
+        object.new_tab($stdin_contents)
+      end
+      object.focus
       exit(0)
       # there is already a Redcar instance running
     end
