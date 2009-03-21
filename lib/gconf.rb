8af65e867f71e04837e732ae117afd5dda2793fd
Daniel Lucraft <dan@fluentradical.com>
2009-03-21 09:45:05 +0000

    Removed a hardcoded path from the GConf registration code

--- lib/gconf.rb
+++ lib/gconf.rb
@@ -1,5 +1,6 @@
 
 
 client = GConf::Client.default
-client["/desktop/gnome/url-handlers/redcar/command"] = "/home/dan/projects/redcar/bin/redcar \"%s\""
+bin_path = File.expand_path(File.dirname(__FILE__) + "/../bin/redcar")
+client["/desktop/gnome/url-handlers/redcar/command"] = "#{bin_path} \"%s\""
 client["/desktop/gnome/url-handlers/redcar/enabled"] = true
