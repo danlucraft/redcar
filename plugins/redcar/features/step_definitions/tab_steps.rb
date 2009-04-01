2bf11ef317a49afa4075acadc719dc8f4d9569bc
Mark H. Wilkinson <mhw@kremvax.net>
2009-04-01 21:29:22 +0100

    Fix pattern to not include the trailing 's' in 'EditTabs'

--- plugins/redcar/features/step_definitions/tab_steps.rb
+++ plugins/redcar/features/step_definitions/tab_steps.rb
@@ -1,5 +1,5 @@
 
-Then /^there should be #{FeaturesHelper::NUMBER_RE} (?:(\w+)s?|tabs?) open$/ do |number, tab_type|
+Then /^there should be #{FeaturesHelper::NUMBER_RE} (?:(\w+?)s?|tabs?) open$/ do |number, tab_type|
   number = parse_number(number)
   if tab_type
     Redcar.win.collect_tabs(Redcar.const_get(tab_type)).length.should == number
