01baa4eb9e0bf5545a930a32a1901d97ddf0b876
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-05-13 13:54:17 +0200

    minor changes

--- plugins/edit_view/lib/auto_complete_word.rb
+++ plugins/edit_view/lib/auto_complete_word.rb
@@ -19,9 +19,9 @@ class Redcar::EditView
     def connect_signals
       #connect_insert_text_signal
       #connect_delete_range_signal
-      @buf.signal_connect("mark_set") do |w, iter, mark|
+      @buf.signal_connect("mark_set") do |document, iter, mark|
         if mark == @buf.cursor_mark
-          @state.cursor_moved
+          @state.cursor_moved(document, iter, mark)
         end
       end
     end
@@ -50,14 +50,32 @@ class Redcar::EditView
     
     # class for Autocomplete state machine
     class AutocompleteState
-      def check_cursor
-        puts "check_cursor called"
+      # determines whether the cursor is currently in a word and
+      # returns the current word if so,
+      # nil otherwise.
+      def cursor_in_word(document, iter, mark)
+        # TODO: only update line content if the line has actually changed!
+        @line = line_at_cursor(document)
+        puts @line
+      end
+      
+      # returns the range that holds the current word (depending on WORD_BOUNDARIES)
+      def word_range
+        
+      end
+      
+      def line_at_cursor(document)
+        line_number = document.cursor_line
+        document.get_slice(document.line_start(line_number), document.line_end(line_number))
       end
     end
     
     class InWordState < AutocompleteState
-      def cursor_moved
-        check_cursor
+      def cursor_moved(document, iter, mark)
+        unless cursor_in_word(document, iter, mark)
+          @state = NotInWordState.new
+          puts @state
+        end
       end
       
       def character_typed
@@ -66,8 +84,11 @@ class Redcar::EditView
     end
     
     class NotInWordState < AutocompleteState
-      def cursor_moved
-        check_cursor
+      def cursor_moved(document, iter, mark)
+        if cursor_in_word(document, iter, mark)
+          @state = InWordState.new
+          puts @state
+        end
       end
       
       def character_typed
