c3005aa2a07c41965a519081772e440e5f85a4cd
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-05-20 09:41:19 +0200

    slightly modified the touched_word method. this will come in handy. also pulled out state machine to seperate file

--- plugins/edit_view/lib/autocompleter.rb
+++ plugins/edit_view/lib/autocompleter.rb
@@ -1,8 +1,9 @@
 
-load File.dirname(__FILE__) + "/autocompleter/word_list.rb"
-load File.dirname(__FILE__) + "/autocompleter/autocomplete_iterator.rb"
 require 'statemachine'
 
+load File.dirname(__FILE__) + "/autocompleter/word_list.rb"
+load File.dirname(__FILE__) + "/autocompleter/autocomplete_iterator.rb"
+load File.dirname(__FILE__) + "/autocompleter/cursor_state_machine.rb"
 
 class Redcar::EditView
   class AutoCompleter
@@ -15,7 +16,8 @@ class Redcar::EditView
       @word_list = WordList.new
       @autocomplete_iterator = AutocompleteIterator.new(buffer, WORD_CHARACTERS)
       buffer.autocompleter = self
-      define_state_machine
+      define_cursor_state_machine
+      define_completion_state_machine
       connect_signals
     end
     
@@ -45,10 +47,10 @@ class Redcar::EditView
     def complete_word
       puts "complete word in AutoCompleteWord called! yay."
       
-      if @cursor_state.context.touching_word
+      prefix, offsets = @cursor_state.context.touched_word
+      if prefix
         # TODO: if repeatedly called this method should NOT requild the list, but toggle through the available completions, if any
         rebuild_word_list
-        prefix = @cursor_state.context.touched_word
         puts "completions for #{prefix} (by distance)"
         @word_list.completions(prefix).each do |completion|
           puts completion
@@ -56,74 +58,8 @@ class Redcar::EditView
       end
     end
     
-    
-    def define_state_machine
-      state_machine = Statemachine.build do
-        trans :in_word_state, :cursor_moved, :check_word_state # start state, event, target state -> check for touching word
-	      trans :not_in_word_state, :cursor_moved, :check_word_state
-	      state :check_word_state do
-          on_entry :check_for_word
-          event :not_in_word, :not_in_word_state
-          event :in_word, :in_word_state
-        end
-      end
+    def define_completion_state_machine
       
-      state_machine.context = AutocompleteCursorStateContext.new(@buf)
-      state_machine.context.statemachine = state_machine
-      @cursor_state = state_machine
     end
-    
-    class AutocompleteCursorStateContext
-      attr_accessor :statemachine, :last_cursor_line, :touched_word
-      
-      def initialize(doc)
-        @last_cursor_line = -1
-        @document = doc
-        puts @document.inspect
-      end
-      
-      def touching_word
-        return nil if @touched_word.length == 0
-        return @touched_word
-      end
-      
-      private
-      def check_for_word
-        @touched_word = word_touching_cursor
-        if @touched_word.length == 0
-          @statemachine.not_in_word
-        else
-          @statemachine.in_word
-        end
-      end
-      
-      # returns the word (see WORD_CHARACTERS) that the cursor is currently touching.
-      # nil otherwise.
-      def word_touching_cursor
-        @line = @document.get_line
-        left, right = word_range
-        @document.get_slice(@document.iter(left), @document.iter(right))
-      end
-      
-      # returns the range that holds the current word (depending on WORD_CHARACTERS)
-      def word_range
-        left = @document.cursor_line_offset - 1
-        right = @document.cursor_line_offset
-        left_range = 0
-        right_range = 0
-        offset = @document.cursor_offset
-        
-        until left == -1 || WORD_CHARACTERS !~ (@line[left].chr)
-          left -= 1
-          left_range -= 1
-        end
-        
-        until right == @line.length || WORD_CHARACTERS !~ (@line[right].chr)
-          right += 1
-          right_range += 1
-        end
-        return [offset+left_range, offset+right_range]
-      end
-    end 
   end
 end
---
+++ plugins/edit_view/lib/autocompleter/cursor_state_machine.rb
@@ -0,0 +1,74 @@
+
+class Redcar::EditView
+  class AutoCompleter
+  
+    # this method and the rest of this file define the state machine context
+    # for the cursor movement tracked by the current autocomplete implementation.
+    def define_cursor_state_machine
+      state_machine = Statemachine.build do
+        trans :in_word_state, :cursor_moved, :check_word_state # start state, event, target state -> check for touching word
+        trans :not_in_word_state, :cursor_moved, :check_word_state
+        state :check_word_state do
+          on_entry :check_for_word
+          event :not_in_word, :not_in_word_state
+          event :in_word, :in_word_state
+        end
+      end
+      
+      state_machine.context = AutocompleteCursorStateContext.new(@buf)
+      state_machine.context.statemachine = state_machine
+      @cursor_state = state_machine
+    end
+
+    class AutocompleteCursorStateContext
+      attr_accessor :statemachine, :last_cursor_line, :touched_word
+      
+      def initialize(doc)
+        @last_cursor_line = -1
+        @document = doc
+        puts @document.inspect
+      end
+      
+      # returns the word that is being touched by the cursor and an array containing [left, right] document offsets of the word.
+      def touched_word
+        @line = @document.get_line
+        left, right = word_range
+        word = @document.get_slice(@document.iter(left), @document.iter(right))
+        return nil if word.length == 0
+        return word, [left, right]
+      end
+      
+      private
+      def check_for_word
+        @touched_word, _ = touched_word
+        if @touched_word
+          @statemachine.in_word
+          puts "in word"
+        else
+          @statemachine.not_in_word
+          puts "not in word"
+        end
+      end
+      
+      # returns the range that holds the current word (depending on WORD_CHARACTERS)
+      def word_range
+        left = @document.cursor_line_offset - 1
+        right = @document.cursor_line_offset
+        left_range = 0
+        right_range = 0
+        offset = @document.cursor_offset
+        
+        until left == -1 || WORD_CHARACTERS !~ (@line[left].chr)
+          left -= 1
+          left_range -= 1
+        end
+        
+        until right == @line.length || WORD_CHARACTERS !~ (@line[right].chr)
+          right += 1
+          right_range += 1
+        end
+        return [offset+left_range, offset+right_range]
+      end
+    end
+  end
+end
