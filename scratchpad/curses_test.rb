07ab9885e6166b398ec841a7c74615f3ec06c773
Daniel Lucraft <danlucraft@The-Oblong-Office.local>
2009-07-14 14:00:55 +0100

    More playing with curses

--- scratchpad/curses_test.rb
+++ scratchpad/curses_test.rb
@@ -1,3 +1,4 @@
+
 =begin
 # Example ncurses program that should print all combinations of foreground
 # color on background color but doesn't work.
@@ -12,6 +13,8 @@ Ncurses.cbreak
 Ncurses.start_color
  
 screen.addstr(Ncurses.COLORS.to_s)
+screen.addstr("  ")
+screen.addstr(Ncurses.COLOR_PAIRS.to_s)
 screen.getch
  
 Ncurses.curs_set 0
@@ -26,9 +29,13 @@ color_count = 0
     color_count += 1
   end
 end
-color_count.times do |i|
-  screen.attrset(Ncurses.COLOR_PAIR(i))
-  screen.addstr("#")
+256.times do |i|
+  Ncurses.clear
+  256.times do |j|
+    screen.attrset(Ncurses.COLOR_PAIR(i*256 + j))
+    screen.addstr("#")
+  end
+  screen.getch
 end
 screen.getch
  
@@ -49,18 +56,43 @@ p :qux
 begin
   color_count = 0
   256.times do |fg|
-    256.times do |bg|
-      Curses.init_pair(color_count, fg, bg)
-      color_count += 1
-    end
+    Curses.init_pair(color_count, fg, 0)
+    color_count += 1
   end
-  color_count.times do |i|
+  # 100.times do |fg|
+  #   Curses.init_pair(color_count, fg, 200)
+  #   color_count += 1
+  # end
+  # 256.times do |fg|
+  #   Curses.init_pair(color_count, 1, fg)
+  #   color_count += 1
+  # end
+  (1*color_count).times do |i|
     Curses.attrset(Curses.color_pair(i))
-    Curses.addstr("#")
+    r = Curses.color_content(i)
+    Curses.addstr(r.inspect)
   end
   Curses.getch
-  
-  # Curses.getch
+    # 
+    # color_count = 0
+    # 256.times do |fg|
+    #   Curses.init_pair(color_count, fg, 256-fg)
+    #   color_count += 1
+    # end
+    # # color_count = 0
+    # # 120.times do |fg|
+    # #   Curses.init_pair(color_count, fg, 0)
+    # #   color_count += 1
+    # # end
+    # # 120.times do |fg|
+    # #   Curses.init_pair(color_count, fg, 80)
+    # #   color_count += 1
+    # # end
+    #   (1s*color_count).times do |i|
+    #     Curses.attrset(Curses.color_pair(i))
+    #     Curses.addstr("#")
+    #   end
+    # Curses.getch
 rescue Object => e
   Curses.close_screen
   puts e.message
