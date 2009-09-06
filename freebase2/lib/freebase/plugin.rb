947738143401ad944c80016a1b405b24fc634687
Daniel Lucraft <dan@fluentradical.com>
2009-09-06 17:30:37 +0100

    FreeBASE adds the plugin lib/ dir to load path

--- freebase2/lib/freebase/plugin.rb
+++ freebase2/lib/freebase/plugin.rb
@@ -207,6 +207,10 @@ module FreeBASE
         raise Exception.new("unment dependencies") unless @plugin_configuration.dependencies_met?
         require @plugin_configuration.dependencies_path if @plugin_configuration.dependencies_path
         log_requires do
+          $LOAD_PATH << File.expand_path(
+              File.join(
+                @plugin_configuration.full_base_path, 
+                "lib"))
           if @plugin_configuration.require_path
             require @plugin_configuration.require_path
           else
--- plugins/core/lib/core.rb
+++ plugins/core/lib/core.rb
@@ -1,18 +1,9 @@
-$LOAD_PATH << File.expand_path(File.dirname(__FILE__))
 
 require "core/plugin"
 
 module Redcar
   class Core
     extend FreeBASE::StandardPlugin
-    
-    def self.load(plugin)
-      plugin.transition(FreeBASE::LOADED)
-    end
-
-    def self.start(plugin)
-      plugin.transition(FreeBASE::RUNNING)
-    end
   end
 end
 
