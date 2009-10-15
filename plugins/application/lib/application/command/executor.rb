a6ecece6d65578185e387078653c4b24e179f301
Daniel Lucraft <dan@fluentradical.com>
2009-10-15 12:20:53 +0100

    Command::Executor sets the Command's environment before executing it

--- plugins/application/lib/application/command/executor.rb
+++ plugins/application/lib/application/command/executor.rb
@@ -1,12 +1,17 @@
 module Redcar
   class Command
     class Executor
+      def self.current_environment
+        {:win => Redcar.app.windows.first}
+      end
+      
       def initialize(command_instance, options={})
         @command_instance = command_instance
         @options          = options
       end
       
       def execute
+        @command_instance.environment(Executor.current_environment)
         @command_instance.execute
         record
       end
--- plugins/application/spec/application/command/executor_spec.rb
+++ plugins/application/spec/application/command/executor_spec.rb
@@ -4,12 +4,18 @@ class Redcar::Command
   describe Executor, "running a command" do
   
     class MyCommand < Redcar::Command
+      def environment(env)
+        $spec_executor_env = env
+      end
+      
       def execute
         $spec_executor_ran_command = true
       end
     end
   
     before do
+      Executor.stub!(:current_environment).and_return(:current_environment)
+      $spec_executor_env         = nil
       $spec_executor_ran_command = false
       Redcar.history = History.new
       @command  = MyCommand.new
@@ -24,6 +30,11 @@ class Redcar::Command
     it "should add the command to the History" do
       @executor.execute
       Redcar.history.last.should == @command
+    end
+    
+    it "should set the command environment" do
+      @executor.execute
+      $spec_executor_env.should == :current_environment
     end
   end
 end
