b0f089e31a240be75adf70455cb3da41c5da5207
Daniel Lucraft <dan@fluentradical.com>
2009-10-15 14:22:29 +0100

    Checks for an EditTab specifically

--- plugins/application_swt/lib/application_swt/tab.rb
+++ plugins/application_swt/lib/application_swt/tab.rb
@@ -1,13 +1,15 @@
 module Redcar
   class ApplicationSWT
     class Tab
+      attr_reader :item
+      
       def initialize(model, notebook)
         @model, @notebook = model, notebook
-        tab_item = Swt::Custom::CTabItem.new(notebook.tab_folder, Swt::SWT::CLOSE)
-        tab_item.text = "Hello!"
+        @item = Swt::Custom::CTabItem.new(notebook.tab_folder, Swt::SWT::CLOSE)
+        @item.text = "Hello!"
         text = Swt::Widgets::Text.new(notebook.tab_folder, Swt::SWT::MULTI)
         text.text = "Big Text"
-        tab_item.control = text
+        @item.control = text
       end
     end
   end
--- plugins/edit_view/features/step_definitions/tab_steps.rb
+++ plugins/edit_view/features/step_definitions/tab_steps.rb
@@ -8,7 +8,8 @@ Then /^there should be one (.*) tab$/ do |tab_type|
   display = Redcar::ApplicationSWT.display
   shell   = display.get_shells.to_a.first
   tab_folder = shell.getChildren.to_a.first
-  p tab_folder.getItems.to_a
-  p Redcar.app.windows.first.notebook.tabs
+  item1 = tab_folder.getItems.to_a.first
+  tab = Redcar.app.windows.first.notebook.tabs.detect{|t| t.controller.item == item1}
   tab_folder.getItems.to_a.length.should == 1
+  tab.should be_an_instance_of tab_class
 end
