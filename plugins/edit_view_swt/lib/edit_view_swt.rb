9e98c7ccd10913fd9c055452f64d40b4da81f25c
Daniel Lucraft <dan@fluentradical.com>
2009-10-17 12:04:52 +0100

    Rearranged loading of JavaMateView

--- plugins/edit_view_swt/lib/edit_view_swt.rb
+++ plugins/edit_view_swt/lib/edit_view_swt.rb
@@ -1,18 +1,6 @@
 require 'edit_view_swt/tab'
 
-Dir[Redcar::ROOT + "/../java-mateview/lib/*.jar"].each do |fn|
-  require fn
-end
-
 require File.dirname(__FILE__) + '/../vendor/java-mateview'
-
-module JavaMateView
-  import com.redcareditor.mate.MateText
-  import com.redcareditor.mate.Grammar
-  import com.redcareditor.mate.Bundle
-  import com.redcareditor.theme.Theme
-  import com.redcareditor.theme.ThemeManager
-end
 
 module Redcar
   class EditViewSWT
--- plugins/edit_view_swt/lib/edit_view_swt/tab.rb
+++ plugins/edit_view_swt/lib/edit_view_swt/tab.rb
@@ -7,10 +7,6 @@ module Redcar
         @model, @notebook = model, notebook
         @item = Swt::Custom::CTabItem.new(notebook.tab_folder, Swt::SWT::CLOSE)
         @item.text = "Hello!"
-        # text = Swt::Widgets::Text.new(notebook.tab_folder, Swt::SWT::MULTI)
-        # text.text = "Example of a tab"
-        # @item.control = text
-        
         
         @contents = Swt::Widgets::Composite.new(notebook.tab_folder, Swt::SWT::NONE)
         @contents.layout = Swt::Layout::FillLayout.new
