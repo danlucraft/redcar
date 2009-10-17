be7d569840fd602853a3f45e9d2eee8e52f20e1f
Daniel Lucraft <dan@fluentradical.com>
2009-10-17 11:48:49 +0100

    EditTabs now contain java-mateview instances.

--- plugins/edit_view_swt/lib/edit_view_swt.rb
+++ plugins/edit_view_swt/lib/edit_view_swt.rb
@@ -1,11 +1,28 @@
 require 'edit_view_swt/tab'
 
+Dir[Redcar::ROOT + "/../java-mateview/lib/*.jar"].each do |fn|
+  require fn
+end
+
+require File.dirname(__FILE__) + '/../vendor/java-mateview'
+
+module JavaMateView
+  import com.redcareditor.mate.MateText
+  import com.redcareditor.mate.Grammar
+  import com.redcareditor.mate.Bundle
+  import com.redcareditor.theme.Theme
+  import com.redcareditor.theme.ThemeManager
+end
+
 module Redcar
   class EditViewSWT
     
     def self.load
       gui = ApplicationSWT.gui
       gui.register_controllers(Redcar::EditTab => EditViewSWT::Tab)
+      
+      JavaMateView::Bundle.load_bundles(Redcar::ROOT + "/../java-mateview/input/")
+      JavaMateView::ThemeManager.load_themes(Redcar::ROOT + "/../java-mateview/input/")
     end
   end
 end
--- plugins/edit_view_swt/lib/edit_view_swt/tab.rb
+++ plugins/edit_view_swt/lib/edit_view_swt/tab.rb
@@ -7,9 +7,18 @@ module Redcar
         @model, @notebook = model, notebook
         @item = Swt::Custom::CTabItem.new(notebook.tab_folder, Swt::SWT::CLOSE)
         @item.text = "Hello!"
-        text = Swt::Widgets::Text.new(notebook.tab_folder, Swt::SWT::MULTI)
-        text.text = "Example of a tab"
-        @item.control = text
+        # text = Swt::Widgets::Text.new(notebook.tab_folder, Swt::SWT::MULTI)
+        # text.text = "Example of a tab"
+        # @item.control = text
+        
+        
+        @contents = Swt::Widgets::Composite.new(notebook.tab_folder, Swt::SWT::NONE)
+        @contents.layout = Swt::Layout::FillLayout.new
+        @mate_text = JavaMateView::MateText.new(@contents)
+        @mate_text.set_grammar_by_name "Ruby"
+        @mate_text.set_theme_by_name "Twilight"
+        @mate_text.set_font "Monaco", 15
+        @item.control = @contents
       end
     end
   end
