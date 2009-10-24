6e9c70914223d8fad9c366004f3a68ace48700af
Daniel Lucraft <dan@fluentradical.com>
2009-10-25 02:47:23 +0800

    Added Tab#close and features close all tabs in between scenarios.

--- plugins/application/lib/application/tab.rb
+++ plugins/application/lib/application/tab.rb
@@ -7,6 +7,18 @@ module Redcar
       @notebook = notebook
     end
     
+    # Close the tab (remove it from the Notebook).
+    #
+    # Events: close
+    def close
+      notify_listeners(:close)
+      has_closed
+    end
+    
+    def has_closed
+      @notebook.tabs.delete(self)
+    end
+    
     # Focus the tab within the notebook, and gives the keyboard focus to the 
     # contents of the tab, if appropriate.
     #
