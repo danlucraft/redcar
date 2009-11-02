5ed1f8185c636c5b40fb5471c17cca6606deb90f
Daniel Lucraft <dan@fluentradical.com>
2009-11-02 14:01:47 +0000

    Minor cleaningg

--- plugins/edit_view_swt/lib/edit_view_swt.rb
+++ plugins/edit_view_swt/lib/edit_view_swt.rb
@@ -8,7 +8,10 @@ module Redcar
     def self.load
       gui = ApplicationSWT.gui
       gui.register_controllers(Redcar::EditTab => EditViewSWT::Tab)
-      
+      load_textmate_assets
+    end
+    
+    def self.load_textmate_assets
       JavaMateView::Bundle.load_bundles(Redcar::ROOT + "/textmate/")
       JavaMateView::ThemeManager.load_themes(Redcar::ROOT + "/textmate/")
       p JavaMateView::Bundle.bundles.to_a.map {|b| b.name }
@@ -20,6 +23,12 @@ module Redcar
     def initialize(model, edit_tab)
       @model = model
       @edit_tab = edit_tab
+      create_mate_text
+      create_document
+      attach_listeners
+    end
+    
+    def create_mate_text
       parent = @edit_tab.notebook.tab_folder
       @widget = Swt::Widgets::Composite.new(parent, Swt::SWT::NONE)
       @widget.layout = Swt::Layout::FillLayout.new
@@ -28,10 +37,12 @@ module Redcar
       @mate_text.set_theme_by_name "Twilight"
       @mate_text.set_font "Monaco", 15
       @edit_tab.item.control = @widget
-      model.controller = self
-      @document = EditViewSWT::Document.new(model.document, @mate_text.document)
-      model.document.controller = @document
-      attach_listeners
+      @model.controller = self
+    end
+    
+    def create_document
+      @document = EditViewSWT::Document.new(@model.document, @mate_text.document)
+      @model.document.controller = @document
     end
     
     def focus
