d00b6f5981d19421807ed01ff6c6724843e28a50
Daniel Lucraft <dan@fluentradical.com>
2009-11-17 14:30:55 +0000

    Added Redcar::Core.platform

--- plugins/core/lib/core.rb
+++ plugins/core/lib/core.rb
@@ -7,6 +7,16 @@ require "core/plugin"
 
 module Redcar
   class Core
+    def self.platform
+      case Config::CONFIG["target_os"]
+      when /darwin/
+        :osx
+      when /mswin/
+        :windows
+      when /linux/
+        :linux
+      end
+    end
   end
 end
 
