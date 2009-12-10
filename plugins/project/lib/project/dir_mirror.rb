d705ab96566b8a3465cc8fa976c3417e32a57784
Daniel Lucraft <dan@fluentradical.com>
2009-12-10 06:26:26 +0000

    More methods implemented on DirMirror: can get top level contents

--- plugins/project/lib/project/dir_mirror.rb
+++ plugins/project/lib/project/dir_mirror.rb
@@ -2,6 +2,7 @@
 module Redcar
   class Project
     class DirMirror
+      include Redcar::TreeView::Mirror
       
       # @param [String] a path to a directory
       def initialize(path)
@@ -11,6 +12,31 @@ module Redcar
       # Does the directory exist?
       def exists?
         File.exist?(@path)
+      end
+      
+      # Have the toplevel nodes changed?
+      def changed?
+        true
+      end
+      
+      def top
+        Dir[@path + "/*"].map {|fn| Node.new(fn)}
+      end
+      
+      class Node
+        include Redcar::TreeView::Mirror::NodeMirror
+        
+        def initialize(path)
+          @path = path
+        end
+        
+        def text
+          File.basename(@path)
+        end
+        
+        def leaf?
+          File.file?(@path)
+        end
       end
     end
   end
--- plugins/project/spec/project/dir_mirror_spec.rb
+++ plugins/project/spec/project/dir_mirror_spec.rb
@@ -2,21 +2,14 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 class Redcar::Project
   describe DirMirror do
-    def write_dir_contents(dirname, files)
-      FileUtils.mkdir_p(dirname)
-      files.each do |filename, contents|
-        File.open(dirname + "/" + filename, "w") {|f| f.print contents}
-      end
-    end
-    
     before do
       @dirname = "project_spec_testdir"
-      @files = {"Carnegie" => "steel", 
-                "Rockefeller" => "oil"}
+      @files = {"Carnegie"    => "steel", 
+                "Rockefeller" => "oil",
+                "subdir"      => {
+                  "Ford" => "cars"
+                }}
       write_dir_contents(@dirname, @files)
-      @dirname2 = "project_spec_testdir/subdir"
-      @files2 = {"Ford" => "cars"}
-      write_dir_contents(@dirname2, @files2)
       @mirror = DirMirror.new(File.expand_path(@dirname))
     end
 
@@ -25,10 +18,45 @@ class Redcar::Project
       it "tells you the directory exists" do
         @mirror.exists?.should be_true
       end
+      
+      it "tells you it has changed" do
+        @mirror.changed?.should be_true
+      end
+      
+      describe "contents" do
+        it "the top nodes are the contents of the directory" do
+          top_nodes = @mirror.top
+          top_nodes.length.should == 3
+          top_nodes.map {|n| n.text}.should == %w(Carnegie Rockefeller subdir)
+        end
+        
+        it "files are leaf nodes" do
+          top_nodes = @mirror.top
+          top_nodes.detect {|n| n.text == "Carnegie"}.leaf?.should be_true
+          top_nodes.detect {|n| n.text == "Rockefeller"}.leaf?.should be_true
+        end
+        
+        it "subdirectories are not leaf nodes" do
+          top_nodes = @mirror.top
+          top_nodes.detect {|n| n.text == "subdir"}.leaf?.should be_false
+        end
+      end
     end
     
     after do
       FileUtils.rm_r(@dirname)
     end
+    
+    def write_dir_contents(dirname, files)
+      FileUtils.mkdir_p(dirname)
+      files.each do |filename, contents|
+        if contents.is_a?(Hash)
+          write_dir_contents(dirname + "/" + filename, contents)
+        else
+          File.open(dirname + "/" + filename, "w") {|f| f.print contents}
+        end
+      end
+    end
+    
   end
 end
