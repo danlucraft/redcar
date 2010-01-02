8cab849eb30f18bb4dfa3495d5323f1edaeeaa03
Daniel Lucraft <dan@fluentradical.com>
2010-01-02 16:26:29 +0000

    Can use the cursor keys to select other items

--- plugins/application_swt/lib/application_swt/dialogs/filter_list_dialog_controller.rb
+++ plugins/application_swt/lib/application_swt/dialogs/filter_list_dialog_controller.rb
@@ -2,7 +2,7 @@ module Redcar
   class ApplicationSWT
     class FilterListDialogController
       class FilterListDialog < Dialogs::NoButtonsDialog
-        attr_reader :list
+        attr_reader :list, :text
         attr_accessor :controller
         
         def createDialogArea(parent)
@@ -14,37 +14,52 @@ module Redcar
           @text.set_layout_data(Swt::Layout::RowData.new(400, 20))
           @list = Swt::Widgets::List.new(composite, Swt::SWT::SINGLE)
           @list.set_layout_data(Swt::Layout::RowData.new(400, 200))
-          attach_listeners
+          controller.attach_listeners
           controller.populate_starting_list
           @list.set_selection(0)
-        end
-        
-        def attach_listeners
-          @text.add_modify_listener do
-            text = @text.get_text
-            controller.text_changed(text)
-          end
-          @text.add_key_listener do |key_event|
-            case key_event.keyCode
-            when Swt::SWT::CR, Swt::SWT::LF
-              controller.selected
-            end
-          end
         end
       end
       
       def initialize(model)
         @model = model
-        attach_listeners
+        @dialog = FilterListDialog.new(Redcar.app.focussed_window.controller.shell)
+        @dialog.controller = self
+        attach_model_listeners
+      end
+      
+      def attach_model_listeners
+        @model.add_listener(:open, &method(:open))
+      end
+
+      class ModifyListener
+        def initialize(controller)
+          @controller = controller
+        end
+        
+        def modify_text(e)
+          @controller.text_changed
+        end
+      end
+      
+      class KeyListener
+        def initialize(controller)
+          @controller = controller
+        end
+        
+        def key_pressed(e)
+        end
+        
+        def key_released(e)
+          @controller.key_pressed(e)
+        end
       end
       
       def attach_listeners
-        @model.add_listener(:open, &method(:open))
+        @dialog.text.add_modify_listener(ModifyListener.new(self))
+        @dialog.text.add_key_listener(KeyListener.new(self))
       end
       
       def open
-        @dialog = FilterListDialog.new(Redcar.app.focussed_window.controller.shell)
-        @dialog.controller = self
         @dialog.open
         @dialog = nil
       end
@@ -53,13 +68,36 @@ module Redcar
         populate_list(@model.starting_list)
       end
       
-      def text_changed(new_text)
-        populate_list(@model.update_list(new_text))
+      def text_changed
+        populate_list(@model.update_list(@dialog.text.get_text))
         @dialog.list.set_selection(0)
       end
       
       def selected
         @model.selected(@dialog.list.get_selection.first)
+      end
+      
+      def key_pressed(key_event)
+        case key_event.keyCode
+        when Swt::SWT::CR, Swt::SWT::LF
+          selected
+        when Swt::SWT::ARROW_DOWN
+          move_down
+        when Swt::SWT::ARROW_UP
+          move_up
+        end
+      end
+      
+      def move_down
+        curr_ix = @dialog.list.get_selection_index
+        new_ix = [curr_ix + 1, @dialog.list.get_item_count - 1].min
+        @dialog.list.set_selection(new_ix)
+      end
+      
+      def move_up
+        curr_ix = @dialog.list.get_selection_index
+        new_ix = [curr_ix - 1, 0].max
+        @dialog.list.set_selection(new_ix)
       end
       
       private
