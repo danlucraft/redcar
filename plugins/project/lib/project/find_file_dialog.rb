2c400dbc1862081431c7180f051360a05bc6b728
Daniel Lucraft <dan@fluentradical.com>
2010-01-16 11:25:12 +0000

    Make FindFileDialog more robust to filenames with unicode in them

--- plugins/project/lib/project/find_file_dialog.rb
+++ plugins/project/lib/project/find_file_dialog.rb
@@ -49,29 +49,35 @@ module Redcar
         score_match_pairs = []
         cutoff = 10000000
 
-        results = files(directories).each do |fn| 
-          unless File.directory?(fn)
-            bit = fn.split("/")
-            if m = bit.last.match(re)
-              cs = []
-              diffs = 0
-              m.captures.each_with_index do |_, i|
-                cs << m.begin(i + 1)
-                if i > 0
-                  diffs += cs[i] - cs[i-1]
+        results = files(directories).each do |fn|
+          begin
+            unless File.directory?(fn)
+              bit = fn.split("/")
+              if m = bit.last.match(re)
+                cs = []
+                diffs = 0
+                m.captures.each_with_index do |_, i|
+                  cs << m.begin(i + 1)
+                  if i > 0
+                    diffs += cs[i] - cs[i-1]
+                  end
                 end
-              end
-              # lower score is better
-              score = (cs[0] + diffs)*100 + bit.last.length
-              if score < cutoff
-                score_match_pairs << [score, fn]
-                score_match_pairs.sort!
-                if score_match_pairs.length == MAX_ENTRIES
-                  cutoff = score_match_pairs.last.first
-                  score_match_pairs.pop
+                # lower score is better
+                score = (cs[0] + diffs)*100 + bit.last.length
+                if score < cutoff
+                  score_match_pairs << [score, fn]
+                  score_match_pairs.sort!
+                  if score_match_pairs.length == MAX_ENTRIES
+                    cutoff = score_match_pairs.last.first
+                    score_match_pairs.pop
+                  end
                 end
               end
             end
+          rescue Errno::ENOENT
+            # File.directory? can throw no such file or directory even if File.exist?
+            # has returned true. For example this happens on some awful textmate filenames
+            # unicode in them.
           end
         end
         score_match_pairs.map {|a| a.last }
