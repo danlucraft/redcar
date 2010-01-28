e9fed706e62ec3b6bbb6ff11da35665596bf7aa4
Daniel Lucraft <dan@fluentradical.com>
2010-01-28 08:35:12 +0000

    Refresh the menu's after reloading a plugin

--- plugins/plugin_manager_ui/lib/plugin_manager_ui.rb
+++ plugins/plugin_manager_ui/lib/plugin_manager_ui.rb
@@ -31,6 +31,8 @@ module Redcar
         def reload_plugin(name)
           plugin = Redcar.plugin_manager.loaded_plugins.detect {|pl| pl.name == name }
           plugin.load
+          Redcar.app.load_menus
+          Redcar.app.refresh_menu!
           PluginManagerUi.last_reloaded = plugin
         end
         
