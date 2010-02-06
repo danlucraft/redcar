5d1191e4575f1a0ce7af1d679fb1b1a8c6f4fd60
Daniel Lucraft <dan@fluentradical.com>
2010-02-06 06:27:25 +0000

    Replaced Text with StyledText in the speedbar, fixes dumb autocomplete OSX thing

--- plugins/application_swt/lib/application_swt/swt_wrapper.rb
+++ plugins/application_swt/lib/application_swt/swt_wrapper.rb
@@ -67,6 +67,7 @@ module Swt
     import org.eclipse.swt.custom.SashForm
     import org.eclipse.swt.custom.StackLayout
     import org.eclipse.swt.custom.ST
+    import org.eclipse.swt.custom.StyledText
   end
   
   module DND
