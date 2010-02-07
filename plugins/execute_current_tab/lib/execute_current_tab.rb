326194d6bbcd04b6bd4e15f8b93556bc41829a5f
rdp <rogerpack2005@gmail.com>
2010-02-06 17:43:43 -0700

    add an 'execute ruby' plugin

---
+++ plugins/execute_current_tab/lib/execute_current_tab.rb
@@ -0,0 +1,48 @@
+module Redcar
+  class ExecuteCurrentTab
+
+    def self.menus
+      Menu::Builder.build do
+        sub_menu "Plugins" do
+          sub_menu "Execute" do
+            item "Execute Current Tab as Ruby", ExecuteCurrentTab::Execute
+            item "Execute Current Tab (within Redcar interpreter)", ExecuteCurrentTab::EmbeddedExecute
+          end
+        end
+      end
+    end
+
+    class Execute < Command
+      key "Ctrl+R"
+
+      def execute
+        doc = win.focussed_notebook_tab_document
+        path = doc.path if doc
+        if path
+          out = `ruby #{path} 2>&1`
+          new_tab = Top::NewCommand.new.run
+          new_tab.document.text = out
+        else
+          puts 'unable to execute'
+        end
+      end
+
+    end
+
+    class EmbeddedExecute < Command
+
+      def execute
+        doc = win.focussed_notebook_tab_document
+        out = doc.get_all_text if doc
+        if out
+          eval out, TOPLEVEL_BINDING, doc.path || doc.title
+        else
+          puts 'unable to execute embedded'
+        end
+      end
+
+    end
+
+  end
+
+end
