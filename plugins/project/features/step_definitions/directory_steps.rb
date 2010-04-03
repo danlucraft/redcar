ec375aa1aceab2543c622b90ed7548594b5db260
Daniel Lucraft <dan@fluentradical.com>
2010-04-03 06:27:16 +0100

    Add a feature to check the project view behaves ok when a directory is removed

--- plugins/project/features/step_definitions/directory_steps.rb
+++ plugins/project/features/step_definitions/directory_steps.rb
@@ -6,3 +6,9 @@ end
 When /^I refresh the directory tree$/ do
   Redcar::Project.refresh_tree(Redcar.app.focussed_window)
 end
+
+When /^I move the myproject fixture away$/ do
+  FileUtils.mv("plugins/project/spec/fixtures/myproject",
+               "plugins/project/spec/fixtures/myproject.bak")
+  @put_myproject_fixture_back = true
+end
