aa08cbbe083a5ca0125de21c94bb93bf971e0245
Antono Vasiljev <antono.vasiljev@gmail.com>
2010-04-06 01:39:58 +0300

    Revert "Tree View: sort items by abc, directories first"
    This reverts commit 3fb919375c614fcd7caae6632f4e1173f04bdc15.
    Sorting should be implemented in plugins, not generic tree.

--- plugins/tree_view_swt/lib/tree_view_swt.rb
+++ plugins/tree_view_swt/lib/tree_view_swt.rb
@@ -2,7 +2,7 @@
 module Redcar
   class TreeViewSWT
     attr_reader :viewer
-
+    
     def self.storage
       @storage ||= begin
          storage = Plugin::Storage.new('tree_view_swt_plugin')
@@ -11,18 +11,19 @@ module Redcar
       end
     end
 
+    
     def initialize(composite, model)
       @composite, @model = composite, model
       @viewer = JFace::Viewers::TreeViewer.new(@composite, Swt::SWT::VIRTUAL)
       @viewer.set_content_provider(TreeMirrorContentProvider.new)
       @viewer.set_input(@model.tree_mirror)
       @viewer.set_label_provider(TreeMirrorLabelProvider.new)
-
+      
       if @model.tree_controller
         @viewer.add_tree_listener(@viewer.getControl, TreeListener.new)
         @viewer.add_open_listener(OpenListener.new(@model.tree_controller))
       end
-
+      
       @model.add_listener(:refresh) do
         s = Time.now
         begin
@@ -37,46 +38,46 @@ module Redcar
         end
       end
     end
-
+    
     def control
       @viewer.getControl
     end
-
+    
     def close
       @viewer.getControl.dispose
     end
-
+    
     class TreeListener
       def tree_collapsed(e)
       end
-
+      
       def tree_expanded(e)
       end
     end
-
+    
     class SelectionListener
       def widget_default_selected(e)
       end
-
+      
       def widget_selected(e)
       end
     end
-
+    
     class DoubleClickListener
       def double_click(e)
       end
     end
-
+    
     class OpenListener
       def initialize(controller)
         @controller = controller
       end
-
+      
       def open(e)
         @controller.activated(e.getSelection.toArray.to_a.first)
       end
     end
-
+    
     class TreeMirrorContentProvider
       include JFace::Viewers::ITreeContentProvider
 
@@ -85,7 +86,7 @@ module Redcar
       end
 
       def get_elements(tree_mirror)
-        sort_elements(tree_mirror.top).to_java
+        tree_mirror.top.to_java
       end
 
       def has_children(tree_node)
@@ -93,21 +94,10 @@ module Redcar
       end
 
       def get_children(tree_node)
-        sort_elements(tree_node.children).to_java
+        tree_node.children.to_java
       end
 
       def dispose
-      end
-
-      private
-
-      def sort_elements(elements)
-        # configurable options here?
-        elements.sort_by do |c|
-          c.path.split(File::SEPARATOR).last
-        end.sort_by do |c|
-          File.directory?(c.path) ? -1 : 1
-        end
       end
     end
 
@@ -135,16 +125,16 @@ module Redcar
 
       def dispose
       end
-
+      
       private
-
+      
       def dir_image
         @dir_image ||= begin
           path = File.join(Redcar.root, %w(plugins application icons darwin-folder.png))
           Swt::Graphics::Image.new(ApplicationSWT.display, path)
         end
       end
-
+      
       def file_image
         @file_image ||= begin
           path = File.join(Redcar.root, %w(plugins application icons darwin-file.png))
