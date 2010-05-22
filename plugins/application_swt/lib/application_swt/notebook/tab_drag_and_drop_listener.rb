2093618192e5b01c0c3df59ccefdab723ffddc53
Tim Felgentreff <timfelgentreff@gmail.com>
2010-05-22 15:13:00 +0200

    fix indicator and allow finer control over moving a tab before or after another

--- plugins/application_swt/lib/application_swt/notebook/tab_drag_and_drop_listener.rb
+++ plugins/application_swt/lib/application_swt/notebook/tab_drag_and_drop_listener.rb
@@ -9,18 +9,21 @@ module Redcar
         # indicators next to a tab in the tab folder
         class TabPaintListener
           include org.eclipse.swt.events.PaintListener    
-          attr_writer :item
+          attr_writer :item, :indicate_front
       
           def paintControl(event)
+            event.gc.background = ApplicationSWT.display.system_color Swt::SWT::COLOR_DARK_GRAY
             if @item && @item.bounds
               bounds = @item.bounds
               side_length = bounds.height / 3
-              triangle = [bounds.x, bounds.y + side_length,
-                bounds.x + side_length, bounds.y,
-                bounds.x - side_length, bounds.y]
-              event.gc.background = ApplicationSWT.display.system_color Swt::SWT::COLOR_DARK_GRAY
-              event.gc.fill_polygon(triangle.to_java(:int))
+              offset = (bounds.width unless @indicate_front) || 0
+              triangle = [bounds.x + offset, bounds.y + side_length,
+                bounds.x + offset + side_length, bounds.y,
+                bounds.x + offset - side_length, bounds.y]
+            else
+              triangle = [0, 0, 10, 10, 0, 20]
             end
+            event.gc.fill_polygon(triangle.to_java(:int))
           end
         end
 
@@ -65,7 +68,9 @@ module Redcar
         # DropTargetListener interface implementation
         # While dragging, update the @link{TabPaintListener} to show drop indicators
         def dragOver(event)
-          @paint_listener.item = event_to_tab_widget(event)
+          widget = event_to_tab_widget(event)
+          @paint_listener.item = widget
+          @paint_listener.indicate_front = move_to_front?(widget, event)
           tab_folder.redraw
         end
 
@@ -77,24 +82,31 @@ module Redcar
             dragged_tab_model = @dragged_tab_controller.model if @dragged_tab_controller
             dragged_tab_model ||= Redcar.app.all_tabs.detect {|t| t.controller.dragging?}
             @notebook.model.grab_tab_from(dragged_tab_model.notebook, dragged_tab_model)
-            move_behind(dragged_tab_model.controller, target_tab_widget)
+            move_tab(dragged_tab_model.controller, target_tab_widget, event)
           end
         end
 
-        # Move the tab controlled by the given controller in behind the indicated tab
+        # Move the tab controlled by the given controller behind or before the indicated tab
         # @param [Redcar::ApplicationSWT::Tab] the dragged tab's controller
         # @param [SWT::Custom::CTabItem] the item to drop behind
-        def move_behind(tab_controller, tab_widget)
+        def move_tab(tab_controller, tab_widget, event)
           position = (tab_folder.index_of(tab_widget) if tab_widget) || 0
+          position -= 1 if move_to_front?(tab_widget, event)
+          position += 1 if tab_folder.index_of(tab_controller.item) > position
           tab_controller.move_tab_widget_to_position(position)
         end
         
         # Find the CTabItem targeted by a given event        
         def event_to_tab_widget(event)
-          if tab_folder.item_count > 0          
+          if tab_folder.item_count > 0
             tab_folder.item(tab_folder.to_control(event.x, event.y)) or 
             tab_folder.items[tab_folder.item_count - 1]
           end
+        end
+        
+        def move_to_front?(widget, event)
+          widget && (widget.bounds.width / 2 > 
+            (tab_folder.to_control(event.x, event.y).x - widget.bounds.x))
         end
 
         # Unimplemented interface methods
