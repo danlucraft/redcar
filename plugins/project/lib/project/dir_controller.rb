2a13ac49a6e79492d4ecfcd4a2d7a51fd3142957
Daniel Lucraft <dan@fluentradical.com>
2010-06-14 20:24:48 +0100

    Refactory DirController and support new files when there is already a file called "New File" in the directory.

--- plugins/project/lib/project/dir_controller.rb
+++ plugins/project/lib/project/dir_controller.rb
@@ -9,48 +9,56 @@ module Redcar
       end
       
       def right_click(tree, node)
-        enclosing_dir = node ? node.directory : tree.tree_mirror.path
+        controller = self
         
         menu = Menu::Builder.build do
-          item("New File") do
-            new_file_path = File.join(enclosing_dir, "New File")
-            FileUtils.touch(new_file_path)
-            tree.refresh
-            tree.expand(node)
-            new_file_node = DirMirror::Node.create_from_path(new_file_path)
-            tree.edit(new_file_node)
-          end
-          
-          item("New Directory") do 
-            new_dir_path = File.join(enclosing_dir, "New Directory")
-            FileUtils.mkdir(new_dir_path)
-            tree.refresh
-            tree.expand(node)
-            new_dir_node = DirMirror::Node.create_from_path(new_dir_path)
-            tree.edit(new_dir_node)
-          end
-          
+          item("New File")      { controller.new_file(tree, node) }
+          item("New Directory") { controller.new_dir(tree, node)  }
           separator
-          
-          item("Rename") do
-            if node.text =~ /^(.*)\.[^\.]+$/
-              tree.edit(node, 0, $1.length)
-            else
-              tree.edit(node)
-            end
-          end
-          
-          item("Delete") do
-            msg = "Really delete #{File.basename(node.path)}?"
-            result = Application::Dialog.message_box(msg, :type => :question, :buttons => :yes_no)
-            if result == :yes
-              FileUtils.rm(node.path)
-              tree.refresh
-            end
-          end
+          item("Rename")        { controller.rename(tree, node)   }
+          item("Delete")        { controller.delete(tree, node)   }
         end
         
         Application::Dialog.popup_menu(menu, :pointer)
+      end
+      
+      def new_file(tree, node)
+        enclosing_dir = node ? node.directory : tree.tree_mirror.path
+        new_file_name = uniq_name(enclosing_dir, "New File")
+        new_file_path = File.join(enclosing_dir, new_file_name)
+        FileUtils.touch(new_file_path)
+        tree.refresh
+        tree.expand(node)
+        new_file_node = DirMirror::Node.create_from_path(new_file_path)
+        tree.edit(new_file_node)
+      end
+      
+      def new_dir(tree, node)
+        enclosing_dir = node ? node.directory : tree.tree_mirror.path
+        new_dir_name = uniq_name(enclosing_dir, "New Directory")
+        new_dir_path = File.join(enclosing_dir, new_dir_name)
+        FileUtils.mkdir(new_dir_path)
+        tree.refresh
+        tree.expand(node)
+        new_dir_node = DirMirror::Node.create_from_path(new_dir_path)
+        tree.edit(new_dir_node)
+      end
+      
+      def rename(tree, node)
+        if node.text =~ /^(.*)\.[^\.]+$/
+          tree.edit(node, 0, $1.length)
+        else
+          tree.edit(node)
+        end
+      end
+      
+      def delete(tree, node)
+        msg = "Really delete #{File.basename(node.path)}?"
+        result = Application::Dialog.message_box(msg, :type => :question, :buttons => :yes_no)
+        if result == :yes
+          FileUtils.rm(node.path)
+          tree.refresh
+        end
       end
       
       def edited(tree, node, text)
@@ -61,6 +69,18 @@ module Redcar
         tree.refresh
         new_node = DirMirror::Node.create_from_path(new_path)
         tree.select(new_node)
+      end
+      
+      private
+      
+      def uniq_name(path, name)
+        return name unless File.exist?(File.join(path, name))
+        i = 1
+        loop do
+          new_name = name + " #{i}"
+          return new_name unless File.exist?(File.join(path, new_name))
+          i += 1
+        end
       end
     end
   end
