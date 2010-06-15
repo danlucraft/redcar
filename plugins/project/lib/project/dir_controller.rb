3757e2991b70bf534b5452e583d17da32758c6d0
Daniel Lucraft <dan@fluentradical.com>
2010-06-15 06:04:09 +0100

    Added Bulk Rename project action.

--- plugins/project/lib/project/dir_controller.rb
+++ plugins/project/lib/project/dir_controller.rb
@@ -15,7 +15,8 @@ module Redcar
           item("New File")      { controller.new_file(tree, node) }
           item("New Directory") { controller.new_dir(tree, node)  }
           separator
-          item("Rename")        { controller.rename(tree, node)   }
+          rename_text = (tree.selection.length > 1 ? "Bulk Rename" : "Rename")
+          item(rename_text)     { controller.rename(tree, node)   }
           item("Delete")        { controller.delete(tree, node)   }
         end
         
@@ -45,10 +46,91 @@ module Redcar
       end
       
       def rename(tree, node)
+        nodes = tree.selection
+        if nodes.length == 1
+          single_rename(tree, node)
+        else
+          bulk_rename(tree, nodes)
+        end
+      end
+      
+      def single_rename(tree, node)
         if node.text =~ /^(.*)\.[^\.]+$/
           tree.edit(node, 0, $1.length)
         else
           tree.edit(node)
+        end
+      end
+      
+      def bulk_rename(tree, nodes)
+        tab = Redcar.app.focussed_window.new_tab(HtmlTab)
+        controller = BulkRenameController.new(tab, tree, nodes)
+        tab.html_view.controller = controller
+        tab.focus
+      end
+      
+      class BulkRenameController
+        attr_reader :pairs, :match_pattern, :replace_pattern
+        
+        def initialize(tab, tree, nodes)
+          @tab = tab
+          @tree = tree
+          @pairs = nodes.map {|node| [node, File.basename(node.path)] }
+          @match_pattern = ""
+          @replace_pattern = ""
+        end
+        
+        def title
+          "Bulk Rename"
+        end
+      
+        def index
+          rhtml = ERB.new(File.read(File.join(File.dirname(__FILE__), "..", "..", "views", "bulk_rename.html.erb")))
+          rhtml.result(binding)
+        end
+        
+        def refresh(new_match_pattern, new_replace_pattern)
+          @match_pattern   = new_match_pattern
+          @replace_pattern = new_replace_pattern
+          result = @pairs.map do |node, _|
+            old_name = File.basename(node.path)
+            new_name = transform_name(old_name)
+            new_path = File.join(File.dirname(node.path), new_name)
+            conflicts = (File.exist?(new_path) and new_name != old_name)
+            legal     = (new_name != "" and legal_path?(new_path))
+            [new_name, conflicts, legal]
+          end
+          result
+        end
+        
+        def submit(params)
+          p [:submit, params]
+          @pairs.each do |node, _|
+            old_name = File.basename(node.path)
+            new_name = transform_name(old_name)
+            new_path = File.join(File.dirname(node.path), new_name)
+            FileUtils.mv(node.path, new_path)
+          end
+          @tab.close
+          @tree.refresh
+        end
+        
+        private
+        
+        def transform_name(old_name)
+          old_name.sub(/#{match_pattern}/, replace_pattern)
+        end
+        
+        def legal_path?(path)
+          return true if File.exist?(path)
+          
+          begin
+            FileUtils.touch(path)
+            FileUtils.rm(path)
+            return true
+          rescue Errno::ENOENT
+            return false
+          end
         end
       end
       
