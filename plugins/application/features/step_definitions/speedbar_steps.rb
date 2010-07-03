60f16a822281526d4307e0856b3418a820b148f9
Daniel Lucraft <dan@fluentradical.com>
2010-07-03 15:22:57 +0100

    Add some speedbar steps.

--- plugins/application/features/step_definitions/speedbar_steps.rb
+++ plugins/application/features/step_definitions/speedbar_steps.rb
@@ -1,5 +1,28 @@
 
-
 Then /^the (.*) speedbar should be open$/ do |class_name|
   Redcar.app.focussed_window.speedbar.class.to_s.should == class_name
+end
+
+When /^I type "([^"]*)" into the "([^"]*)" field in the speedbar$/ do |text, field_name|
+  speedbar = Redcar.app.focussed_window.speedbar
+  speedbar.send(field_name).edit_view.document.text = text
+end
+
+When /^I press "([^"]*)" in the speedbar$/ do |button_name|
+  speedbar = Redcar.app.focussed_window.speedbar
+  speedbar.controller.execute_listener_in_model(speedbar.send(button_name))
+end
+
+When /^I check "([^"]*)" in the speedbar$/ do |checkbox_name|
+  speedbar = Redcar.app.focussed_window.speedbar
+  item = speedbar.send(checkbox_name)
+  item.set_value(true)
+  speedbar.controller.execute_listener_in_model(item, true)
+end
+
+When /^I uncheck "([^"]*)" in the speedbar$/ do |checkbox_name|
+  speedbar = Redcar.app.focussed_window.speedbar
+  item = speedbar.send(checkbox_name)
+  item.set_value(false)
+  speedbar.controller.execute_listener_in_model(item, false)
 end
