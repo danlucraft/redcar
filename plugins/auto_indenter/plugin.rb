2393e5fcec3845c5a9ccc38275d3fd4794a6694e
Daniel Lucraft <dan@fluentradical.com>
2010-07-25 14:10:17 +0100

    Fix the autopairer not working (there's got to be a better way of sorting out priorities among DocumentControllers)

--- plugins/auto_indenter/plugin.rb
+++ plugins/auto_indenter/plugin.rb
@@ -4,5 +4,6 @@ Plugin.define do
   version "1.0"
   file    "lib", "auto_indenter"
   object  "Redcar::AutoIndenter"
-  dependencies "edit_view", ">0"
+  dependencies "edit_view", ">0",
+               "auto_pairer", ">0"
 end
