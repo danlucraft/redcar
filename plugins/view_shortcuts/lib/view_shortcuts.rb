33f4b4e8a3c13bc4d13bf18013f0c744f6a15d3d
delisa <iskanamagus@gmail.com>
2010-07-28 21:45:06 -0400

    added keyboard shortcut view plugin. displays keyboard shortcuts in a HtmlTab

---
+++ plugins/view_shortcuts/lib/view_shortcuts.rb
@@ -0,0 +1,37 @@
+
+require 'erb'
+require 'cgi'
+
+module Redcar
+  class ViewShortcuts
+    def self.menus
+      Menu::Builder.build do
+        sub_menu "Plugins" do
+          item "View Shortcuts", ViewShortcuts::ViewCommand
+        end
+      end
+    end
+  
+    class ViewCommand < Redcar::Command
+      def execute
+        controller = Controller.new
+        tab = win.new_tab(HtmlTab)
+        tab.html_view.controller = controller
+        tab.focus
+      end
+    end
+    
+    class Controller
+      include HtmlController
+      
+      def title
+        "Shortcuts"
+      end
+      
+      def index
+        rhtml = ERB.new(File.read(File.join(File.dirname(__FILE__), "..", "views", "index.html.erb")))
+        rhtml.result(binding)
+      end
+    end
+  end
+end
