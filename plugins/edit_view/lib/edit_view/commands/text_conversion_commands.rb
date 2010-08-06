cffb4ad42e7df927647bd93d452fc76abbb6a24f
Daniel Lucraft <dan@fluentradical.com>
2010-08-06 08:51:42 +0100

    Style changes in the text conversion commands, and update CHANGES

--- plugins/edit_view/lib/edit_view/commands/text_conversion_commands.rb
+++ plugins/edit_view/lib/edit_view/commands/text_conversion_commands.rb
@@ -1,67 +1,62 @@
 module Redcar
   class EditView
+    # Subclass and implement a 'convert/1 method.'
     class TextConversionCommand < Redcar::DocumentCommand
       # Calls Document#replace_selection if something is highlighted
       # otherwise calls Document#replace_word_at_offset passing
       # Document#cursor_offset as the offset
-      def replace_selection_or_word_at_cursor(new_text=nil, &block)
+      def execute(new_text=nil, &block)
         if doc.selection?
-          doc.replace_selection(new_text, &block)
+          doc.replace_selection(new_text, &method(:convert))
         else
-          doc.replace_word_at_offset(doc.cursor_offset, new_text, &block)
+          doc.replace_word_at_offset(doc.cursor_offset, new_text, &method(:convert))
         end
       end      
     end
     
     class UpcaseTextCommand < TextConversionCommand
-      def execute
-        replace_selection_or_word_at_cursor(&:upcase)
+      def convert(text)
+        text.upcase
       end
     end
     
     class DowncaseTextCommand < TextConversionCommand      
-      def execute
-        replace_selection_or_word_at_cursor(&:downcase)
+      def convert(text)
+        text.downcase
       end
     end
     
     class TitlizeTextCommand < TextConversionCommand
-      def execute
-        replace_selection_or_word_at_cursor do |text|
-          text.gsub(/\b('?[a-z])/) { $1.capitalize }
-        end
+      def convert(text)
+        text.gsub(/\b('?[a-z])/) { $1.capitalize }
       end
     end
     
     class OppositeCaseTextCommand < TextConversionCommand
-      def execute
-        replace_selection_or_word_at_cursor do |text|
-          text.tr('a-zA-Z', 'A-Za-z')
-        end
+      def convert(text)
+        text.tr('a-zA-Z', 'A-Za-z')
       end
     end
     
     class OppositeCaseTextCommand < TextConversionCommand
-      def execute
-        replace_selection_or_word_at_cursor do |text|
-          text.tr('a-zA-Z', 'A-Za-z')
-        end
+      def convert(text)
+        text.tr('a-zA-Z', 'A-Za-z')
       end
     end
 
     class CamelCaseTextCommand < TextConversionCommand
-      def execute
-        replace_selection_or_word_at_cursor(&:camelize)
+      def convert(text)
+        text.camelize
       end
     end
     
     class UnderscoreTextCommand < TextConversionCommand
-      def execute
-        replace_selection_or_word_at_cursor(&:lower_case_underscore)
+      def convert(text)
+        text.lower_case_underscore
       end
     end
     
-    # Blantantly taken from the "Toggle camelCase / snake_case / PascalCase" Command
+    # Ported from "Toggle camelCase / snake_case / PascalCase" Command
     # in TextMate's Source bundle written by Allan Odgaard.
     class CamelSnakePascalRotateTextCommand < TextConversionCommand
       # HotFlamingCats -> hot_flaming_cats
@@ -77,20 +72,18 @@ module Redcar
       # hotFlamingCats -> HotFlamingCats
       def camelcase_to_pascalcase(word)
         word.gsub(/^\w{1}/) {|c| c.upcase}
-      end      
+      end
 
-      def execute
-        replace_selection_or_word_at_cursor do |word|
-          is_pascal = word.match(/^[A-Z]{1}/) ? true : false
-          is_snake = word.match(/_/) ? true : false
+      def convert(word)
+        is_pascal = word.match(/^[A-Z]{1}/) ? true : false
+        is_snake = word.match(/_/) ? true : false
 
-          if is_pascal then
-            pascalcase_to_snakecase(word)
-          elsif is_snake then
-  	        snakecase_to_camelcase(word)
-          else
-            camelcase_to_pascalcase(word) 
-          end
+        if is_pascal
+          pascalcase_to_snakecase(word)
+        elsif is_snake
+	        snakecase_to_camelcase(word)
+        else
+          camelcase_to_pascalcase(word) 
         end
       end
     end
