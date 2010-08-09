6f48ed8214226a769ffb26d9ce94bd7244e2c728
Mat Schaffer <mat.schaffer@gmail.com>
2010-08-08 23:19:14 -0400

    Woo! Got some colors in a cucumber feature!
    Just make sure to add --color to the cuke command. Otherwise it will
    suppress color since it's not going to a terminal.

--- plugins/runnables/lib/runnables/command_output_controller.rb
+++ plugins/runnables/lib/runnables/command_output_controller.rb
@@ -49,11 +49,20 @@ module Redcar
         end
       end
       
+      def ansi_to_html(str)
+        str.gsub(/\e\[32m/,      '<span class="ansi-green">').
+            gsub(/\e\[90m/,      '<span class="ansi-gray">').
+            gsub(/\e\[(0;){0,1}31m/, '<span class="ansi-red">').
+            gsub(/\e\[1m/,       '<span class="ansi-bold">').
+            gsub(/\e\[0m/,       '</span>')
+      end
+      
       # From TextMate's Support/lib/escape.rb
       # Make string suitable for display as HTML, preserve spaces. Set :no_newline_after_br => true
       # to cause “\n” to be substituted by “<br>” instead of “<br>\n”
       def htmlize(str, opts = {}) 
         str = str.to_s.gsub("&", "&amp;").gsub("<", "&lt;")
+        str = ansi_to_html(str)
         str = str.gsub(/\t+/, '<span style="white-space:pre;">\0</span>')
         str = str.reverse.gsub(/ (?= |$)/, ';psbn&').reverse
         if opts[:no_newline_after_br].nil?
