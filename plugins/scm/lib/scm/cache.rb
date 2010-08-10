034362d57a23aead99f32d18ff3ee37bc8761530
Matthew Scharley <matt@scharley.me>
2010-08-10 18:56:10 +1000

    Start on a caching object to provide cached results from the repository.

---
+++ plugins/scm/lib/scm/cache.rb
@@ -0,0 +1,12 @@
+
+# This is a class used to cache data from the repository, so that it 
+# needn't be queried over and over. Use a timeout of a few seconds to
+# prevent repeated queries in the same operation, but preserve the 
+# ability to retrieve up to date data.
+module Redcar
+  module Scm
+    class Cache
+      
+    end
+  end
+end
