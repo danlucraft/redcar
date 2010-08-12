07d23b2fe2888388f3e6d3738c5a980d2e9b45d4
Matthew Scharley <matt@scharley.me>
2010-08-12 13:40:36 +1000

    Updated the cache to allow arguments to the block. This means each set of unique arguments generates a new cache object.

--- plugins/scm/lib/scm/cache.rb
+++ plugins/scm/lib/scm/cache.rb
@@ -13,12 +13,18 @@
 #   @cache ||= begin
 #     c = Cache.new
 #     c.add("test", 10) { "Hello world!" }
+#     # This block always returns the same thing, so never refresh it
+#     c.add("test2") { |name| "Hello #{name}" }
 #     c
 #   end
 # end
+#
+# cache["test2", "Matthew"] # "Hello Matthew"
 module Redcar
   module Scm
     class Cache
+      
+      #attr_reader :values, :accessed
       
       def initialize(default=nil)
         @default_value = default
@@ -28,39 +34,89 @@ module Redcar
         @values = {}
       end
       
-      def add(name, timeout, &block)
+      def add(name, timeout=nil, &block)
         @blocks[name] = block
         @timeouts[name] = timeout
-        @accessed[name] = Time.at(0)
       end
       
-      def [](name)
+      def [](name, *args)
         return @default_value if not @blocks.keys.include?(name)
         
-        atime = @accessed[name]
+        atime = get_accessed(@accessed, [name, *args])
         ctime = Time.now
         
-        if (ctime - atime > @timeouts[name])
+        if ((not @timeouts[name].nil?) or (not value_exist?(@values, [name, *args]))) and ctime - atime > (@timeouts[name] || 0)
           begin
-            @values[name] = @blocks[name].call
+            set_value(@values, @blocks[name].call(*args), [name, *args])
           rescue
             # dump errors to the console, but otherwise, keep on trucking
             puts $!.backtrace
-            @values[name] = @default_value
+            set_value(@values, @default_value, [name, *args])
           end
           
-          @accessed[name] = ctime
+          set_accessed(@accessed, ctime, [name, *args])
         end
         
-        @values[name]
+        get_value(@values, [name, *args])
       end
       
-      def refresh(name=nil)
-        keys = name ? [name] : @accessed.keys
+      def refresh(times=@accessed)
+        times.each do |k, v|
+          if v.respond_to? '[]'
+            refresh v
+          else
+            times[k] = Time.at(0)
+          end
+        end
+      end
+      
+      private
+      
+      def value_exist?(values, path)
+        while path.length > 1
+          curr = path.shift
+          values = values[curr]
+          return false if not values.respond_to? '[]'
+        end
         
-        keys.each {|k| @accessed[k] = Time.at(0)}
+        return values.keys.include?(path[0])
       end
       
+      def get_value(values, path)
+        get_from_path(values, path, @default_value)
+      end
+      
+      def get_accessed(times, path)
+        get_from_path(times, path, Time.at(0))
+      end
+      
+      def get_from_path(hash, path, default)
+        while path.length > 1
+          curr = path.shift
+          hash = hash[curr]
+          return default if not hash.respond_to? '[]'
+        end
+        
+        return hash[path[0]] || default
+      end
+      
+      def set_value(values, value, path)
+        set_from_path(values, value, path)
+      end
+      
+      def set_accessed(times, time, path)
+        set_from_path(times, time, path)
+      end
+      
+      def set_from_path(hash, value, path)
+        while path.length > 1
+          curr = path.shift
+          hash[curr] ||= {}
+          hash = hash[curr]
+        end
+        
+        hash[path[0]] = value
+      end
     end
   end
 end
