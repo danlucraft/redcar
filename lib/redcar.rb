91990f0aabd39639fc259be9d54f204ec98c86b0
Matthew Scharley <matt@scharley.me>
2010-08-13 09:02:34 +1000

    Added a rescue clause around the fork code so that systems that don't support this (I'm looking at you Windows!) will still start up properly.

--- lib/redcar.rb
+++ lib/redcar.rb
@@ -68,26 +68,31 @@ module Redcar
     jruby = Config::CONFIG["RUBY_INSTALL_NAME"] == "jruby"
     osx = ! [:linux, :windows].include?(platform)    
 
-    if forking and not jruby
-      # jRuby doesn't support fork() because of the runtime stuff...
-      forking = false
-      puts 'Forking failed, attempting to start anyway...' if (pid = fork) == -1
-      exit unless pid.nil?
-
-      if pid.nil?
-        # reopen the standard pipes to nothingness
-        STDIN.reopen '/dev/null'
-        STDOUT.reopen '/dev/null', 'a'
-        STDERR.reopen STDOUT
+    begin
+      if forking and not jruby
+        # jRuby doesn't support fork() because of the runtime stuff...
+        forking = false
+        puts 'Forking failed, attempting to start anyway...' if (pid = fork) == -1
+        exit unless pid.nil? # kill the parent process
+        
+        if pid.nil?
+          # reopen the standard pipes to nothingness
+          STDIN.reopen '/dev/null'
+          STDOUT.reopen '/dev/null', 'a'
+          STDERR.reopen STDOUT
+        end
+      elsif forking
+        # so we need to try something different...
+        # Need to work out the vendoring stuff here.
+        # for now just blow up and do what we'd normally do.
+        #require 'spoon'
+        raise NotImplementedError, 'Attempting to fork from inside jRuby. jRuby doesn\'t support this.'
+        #puts 'Continuing normally...'
+        #forking = false
       end
-    elsif forking
-      # so we need to try something different...
-      # Need to work out the vendoring stuff here.
-      # for now just blow up and do what we'd normally do.
-      #require 'spoon'
-      puts 'Attempting to fork from inside jRuby. jRuby doesn\'t support this.'
-      puts 'Continuing normally...'
-      forking = false
+    rescue e => NotImplementedError
+      puts "Forking isn't supported on this system. Sorry."
+      puts "Starting normally..."
     end
     
     return if no_runner
