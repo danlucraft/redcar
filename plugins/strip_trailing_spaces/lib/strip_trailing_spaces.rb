8b770195373b44b0162f935f4422baa59ce6f4b0
Steven Hancock <stevenh512@gmail.com>
2010-08-16 09:14:33 -0700

    Make auto-pairer ignore Save Trailing Spaces line changes.

--- plugins/strip_trailing_spaces/lib/strip_trailing_spaces.rb
+++ plugins/strip_trailing_spaces/lib/strip_trailing_spaces.rb
@@ -26,13 +26,16 @@ module Redcar
     def self.before_save(doc)
       if (doc.mirror.is_a?(Redcar::Project::FileMirror) && storage['enabled'])
         indenter = doc.controllers(Redcar::AutoIndenter::DocumentController)[0]
+        pairer = doc.controllers(Redcar::AutoPairer::DocumentController)[0]
         indenter = nil unless indenter.is_a?(Redcar::AutoIndenter::DocumentController)
 
-        indenter.increase_ignore if indenter != nil
-        doc.line_count.times do |l|
-          doc.replace_line(l) { |line_text| line_text.rstrip }
+        pairer.ignore do
+          indenter.increase_ignore if indenter != nil
+          doc.line_count.times do |l|
+            doc.replace_line(l) { |line_text| line_text.rstrip }
+          end
+          indenter.decrease_ignore if indenter != nil
         end
-        indenter.decrease_ignore if indenter != nil
       end
     end
 
