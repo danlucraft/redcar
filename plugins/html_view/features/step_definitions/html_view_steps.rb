976e80734de10a0d6ede7ffc34e01b5dd6d4020e
Mat Schaffer <mat.schaffer@gmail.com>
2010-08-18 19:37:36 -0400

    Wrote an html contents step that almost works, but it stalls the cukes at the end.

--- plugins/html_view/features/step_definitions/html_view_steps.rb
+++ plugins/html_view/features/step_definitions/html_view_steps.rb
@@ -1,9 +1,30 @@
 require 'ruby-debug'
 
-Then /^the HTML tab should say "([^"]*)"$/ do |content| 
+Then /^the HTML tab should say "([^"]*)"$/ do |needle|
+  limit = 5
+  @thread = Thread.new(limit, focussed_tab.html_view.controller) do |limit, c|
+    start = Time.now
+    
+    contents = get_browser_contents
+    now = start
+    while !(match = contents.match(needle)) && now - start < limit
+      contents = get_browser_contents
+      sleep 0.1
+      now = Time.now
+      puts "Looping at #{now}"
+    end
+    
+    
+    puts "Unable to find #{needle} in #{limit} seconds in contents:\n #{contents}" if !match
+    Redcar.gui.stop
+  end
+
+  Redcar.gui.stop
+  Redcar.gui.start
+  puts "cuke continued"
+  
   # Here is where I'd love to say browser.eval($(body).text()
   # But unfortunately the UI is already blocked at this point
-
   #@thread = Thread.new(focussed_tab) do |t|
   #  while true
   #    begin
