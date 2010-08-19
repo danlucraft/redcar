d32fa11ba097de63d0e0032cd0a27ca82976e596
delisa <iskanamagus@gmail.com>
2010-08-18 21:18:38 -0400

    Moved commands to separate file - fixes install errors.

--- plugins/textmate/lib/textmate.rb
+++ plugins/textmate/lib/textmate.rb
@@ -6,6 +6,7 @@ require 'textmate/preference'
 require 'textmate/snippet'
 begin
   require 'textmate/tree_mirror'
+  require 'textmate/commands'
 rescue NameError => e
   # In the installer we don't have all of core loaded yet
 end
@@ -34,9 +35,8 @@ module Redcar
             item ("Remove from Bundles Menu") do
               RemovePinnedBundle.new(node.text).run
             end
-          end
-          if not Textmate.storage['loaded_bundles'].include?(node.text.downcase)
-            item ("Pin to Bundles Menu") do
+          else
+            item("Pin to Bundles Menu") do
               PinBundleToMenu.new(node.text).run  
             end
           end
@@ -146,69 +146,6 @@ module Redcar
         end
         res
       end
-    end
-    
-    class ClearBundleMenu < Redcar::Command
-      def execute
-        Textmate.storage['loaded_bundles'] = []
-        Redcar.app.refresh_menu!
-      end
-    end
-    
-    class RemovePinnedBundle < Redcar::Command
-      def initialize(bundle_name)
-        @bundle_name = bundle_name.downcase
-      end
-      
-      def execute 
-        unless not Textmate.storage['loaded_bundles'].include?(@bundle_name)
-          bundles = Textmate.storage['loaded_bundles'] || []
-          bundles.delete(@bundle_name)
-          Textmate.storage['loaded_bundles'] = bundles
-          Redcar.app.refresh_menu!
-        end
-      end
-    end
-    
-    class PinBundleToMenu < Redcar::Command
-      def initialize(bundle_name)
-        @bundle_name = bundle_name.downcase
-      end
-      
-      def execute
-        unless Textmate.storage['loaded_bundles'].include?(@bundle_name)
-          bundles = Textmate.storage['loaded_bundles'] || []
-          bundles << @bundle_name
-          Textmate.storage['loaded_bundles'] = bundles
-          Redcar.app.refresh_menu!
-        end
-      end
-    end
-
-    begin
-      class InstalledBundles < Redcar::Command
-        def execute
-          controller = Controller.new
-          tab = win.new_tab(HtmlTab)
-          tab.html_view.controller = controller
-          tab.focus
-        end
-
-        class Controller
-          include Redcar::HtmlController
-
-          def title
-            "Installed Bundles"
-          end
-
-          def index
-            rhtml = ERB.new(File.read(File.join(File.dirname(__FILE__), "..", "views", "installed_bundles.html.erb")))
-            rhtml.result(binding)
-          end
-        end
-      end
-    rescue NameError => e
-      puts "Delaying full textmate plugin while installing."
     end
   end
 end
---
+++ plugins/textmate/lib/textmate/commands.rb
@@ -0,0 +1,68 @@
+
+module Redcar
+  module Textmate
+       
+    class ClearBundleMenu < Redcar::Command
+      def execute
+        Textmate.storage['loaded_bundles'] = []
+        Redcar.app.refresh_menu!
+      end
+    end
+    
+    class RemovePinnedBundle < Redcar::Command
+      def initialize(bundle_name)
+        @bundle_name = bundle_name.downcase
+      end
+      
+      def execute 
+        unless not Textmate.storage['loaded_bundles'].include?(@bundle_name)
+          bundles = Textmate.storage['loaded_bundles'] || []
+          bundles.delete(@bundle_name)
+          Textmate.storage['loaded_bundles'] = bundles
+          Redcar.app.refresh_menu!
+        end
+      end
+    end
+    
+    class PinBundleToMenu < Redcar::Command
+      def initialize(bundle_name)
+        @bundle_name = bundle_name.downcase
+      end
+      
+      def execute
+        unless Textmate.storage['loaded_bundles'].include?(@bundle_name)
+          bundles = Textmate.storage['loaded_bundles'] || []
+          bundles << @bundle_name
+          Textmate.storage['loaded_bundles'] = bundles
+          Redcar.app.refresh_menu!
+        end
+      end
+    end
+
+    begin
+      class InstalledBundles < Redcar::Command
+        def execute
+          controller = Controller.new
+          tab = win.new_tab(HtmlTab)
+          tab.html_view.controller = controller
+          tab.focus
+        end
+
+        class Controller
+          include Redcar::HtmlController
+
+          def title
+            "Installed Bundles"
+          end
+
+          def index
+            rhtml = ERB.new(File.read(File.join(File.dirname(__FILE__), "..", "views", "installed_bundles.html.erb")))
+            rhtml.result(binding)
+          end
+        end
+      end
+    rescue NameError => e
+      puts "Delaying full textmate plugin while installing."
+    end 
+  end
+end
