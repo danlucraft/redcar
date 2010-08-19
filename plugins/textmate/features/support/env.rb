0bc89e018323b2b09ffdb87a2d34fe8cfbfd33fc
delisa <iskanamagus@gmail.com>
2010-08-19 15:15:47 -0400

    cleanup

--- plugins/textmate/features/support/env.rb
+++ plugins/textmate/features/support/env.rb
@@ -10,9 +10,6 @@ def tmp_bundle_path_1
   File.expand_path(File.dirname(__FILE__) + "/../../vendor/redcar-bundles/Bundles/test_bundle.tmbundle")
 end
 
-def reset_tmp_bundles
-end
-
 Before do
   FileUtils.rm_rf tmp_bundle_path_1
 end
