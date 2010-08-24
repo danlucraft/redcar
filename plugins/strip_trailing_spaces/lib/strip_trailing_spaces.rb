c9dcbff9ba338bb1a75e180cdf094840bce9fb43
Steven Hancock <stevenh512@gmail.com>
2010-08-23 23:52:31 -0700

    Restored cursor handling in Strip Trailing Spaces
    Fixes #288

--- plugins/strip_trailing_spaces/lib/strip_trailing_spaces.rb
+++ plugins/strip_trailing_spaces/lib/strip_trailing_spaces.rb
@@ -1,4 +1,3 @@
-
 module Redcar
   class StripTrailingSpaces
     def self.storage
@@ -25,6 +24,7 @@ module Redcar
 
     def self.before_save(doc)
       if (doc.mirror.is_a?(Redcar::Project::FileMirror) && storage['enabled'])
+        cursor = CursorHandler.new(doc)
         indenter = doc.controllers(Redcar::AutoIndenter::DocumentController)[0]
         pairer = doc.controllers(Redcar::AutoPairer::DocumentController)[0]
         indenter = nil unless indenter.is_a?(Redcar::AutoIndenter::DocumentController)
@@ -38,6 +38,30 @@ module Redcar
           end
           indenter.decrease_ignore if indenter != nil
         end
+
+        cursor.adjust
+      end
+    end
+
+    class CursorHandler
+      ##
+      # Read cursor position and adjust line offset
+      def initialize(doc)
+        @doc = doc
+        @cursor_line = @doc.cursor_line
+        @top_line = @doc.smallest_visible_line
+        @line_offset = @doc.cursor_line_offset
+        line = @doc.get_line(@cursor_line)
+        @line_offset = line.rstrip.size if @line_offset > line.rstrip.size
+      end
+
+      ##
+      # Adjust cursor offset and make visible
+      def adjust
+        @doc.scroll_to_line_at_top(@top_line)
+        offset=@doc.offset_at_line(@cursor_line) + @line_offset
+        @doc.cursor_offset=offset
+        @doc.ensure_visible(offset)
       end
     end
 
