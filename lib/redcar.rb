304ebe7d2a1e0ffeed218352966c20643e1bb052
Matthew Scharley <matt@scharley.me>
2010-08-27 20:18:53 +1000

    Implemented the spoon gem into the --fork option.
    We can fork from jRuby now on systems that support the posix_spawn() C function.

--- lib/redcar.rb
+++ lib/redcar.rb
@@ -1,5 +1,7 @@
 
-$:.push File.expand_path(File.join(File.dirname(__FILE__)))
+$:.push(File.expand_path(File.join(File.dirname(__FILE__))))
+$:.push(File.expand_path(File.join(File.dirname(__FILE__), %w{.. vendor spoon lib})))
+$:.push(File.expand_path(File.join(File.dirname(__FILE__), %w{.. vendor ffi lib})))
 
 require 'redcar/usage'
 
@@ -13,6 +15,17 @@ require 'regex_replace'
 require 'forwardable'
 require 'yaml'
 require 'uri'
+
+begin
+  if Config::CONFIG["RUBY_INSTALL_NAME"] == "jruby"
+    require 'spoon'
+    module Redcar; SPOON_AVAILABLE = true; end
+  else
+    throw LoadError, "Only load spoon in jruby."
+  end
+rescue LoadError
+  module Redcar; SPOON_AVAILABLE = false; end
+end
 
 # ## Loading and Initialization
 #
@@ -68,7 +81,7 @@ module Redcar
     forking = ARGV.include?("--fork")
     no_runner = ARGV.include?("--no-sub-jruby")
     jruby = Config::CONFIG["RUBY_INSTALL_NAME"] == "jruby"
-    osx = ! [:linux, :windows].include?(platform)    
+    osx = (not [:linux, :windows].include?(platform))
 
     begin
       if forking and not jruby
@@ -83,16 +96,22 @@ module Redcar
           STDOUT.reopen '/dev/null', 'a'
           STDERR.reopen STDOUT
         end
-      elsif forking
+      elsif forking and SPOON_AVAILABLE and ::Spoon.supported?
         # so we need to try something different...
-        # Need to work out the vendoring stuff here.
-        # for now just blow up and do what we'd normally do.
-        #require 'spoon'
-        raise NotImplementedError, 'Attempting to fork from inside jRuby. jRuby doesn\'t support this.'
-        #puts 'Continuing normally...'
-        #forking = false
+        
+        forking = false
+        require 'redcar/runner'
+        runner = Redcar::Runner.new
+        runner.spin_up do |command|
+          command.push('--forked')
+          ::Spoon.spawnp(*command)
+        end
+        exit 0
+      elsif forking
+        raise NotImplementedError, "Something weird has happened. Please contact us."
       end
     rescue NotImplementedError
+      puts $!.class.name + ": " + $!.message
       puts "Forking isn't supported on this system. Sorry."
       puts "Starting normally..."
     end
@@ -102,7 +121,9 @@ module Redcar
     
     require 'redcar/runner'
     runner = Redcar::Runner.new
-    runner.spin_up
+    runner.spin_up do |command|
+      exec(*command)
+    end
   end
 
   def self.root
