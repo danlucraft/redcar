b6f8107b353fbe6ef08033057b397288d44cbbb4
Alex Lourie <djay.il@gmail.com>
2010-09-07 17:07:37 +0300

    Fixed Linux GTK assertion errors, fixed jvm flags discovery

--- lib/redcar/jvm_options_probe.rb
+++ lib/redcar/jvm_options_probe.rb
@@ -1,14 +1,24 @@
+require "open3"
 
 class JvmOptionsProbe
+
   def initialize
-    @help = `java -help`
+    @d32 = @d64 = @client = false
+    @help = Open3.popen3("java -h")[2].readlines
+    @d32 = (@help == Open3.popen3("java -d32")[2].readlines) ? true : false
+    @d64 = (@help == Open3.popen3("java -d64")[2].readlines) ? true : false
+    @client = (@help == Open3.popen3("java -client")[2].readlines) ? true : false
   end
   
   def can_use_d32?
-    @help.include?("-d32")
+    @d32
+  end
+  
+  def can_use_d64?
+    @d64
   end
   
   def can_use_client?
-    @help.include?("-client")
+    @client
   end
 end
