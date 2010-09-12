4c57e614ee2dd66fb9c26046f2cc3eb9517bf179
Daniel Lucraft <dan@fluentradical.com>
2010-09-12 17:29:30 +0100

    Added ApplicationSWT::Icon helper class for generating Swt image icons.

---
+++ plugins/application_swt/lib/application_swt/icon.rb
@@ -0,0 +1,37 @@
+
+module Redcar
+  class ApplicationSWT
+    module Icon
+      def self.swt_image(icon)
+        case icon
+        when :directory
+          dir_image
+        when :file
+          file_image
+        when Symbol
+          image(File.expand_path(File.join(Redcar::ICONS_DIRECTORY, icon.to_s + ".png")))
+        when String
+          image(icon)
+        end
+      end
+      
+      def self.image(path)
+        cached_images[path] ||= Swt::Graphics::Image.new(ApplicationSWT.display, path)
+      end
+      
+      def self.cached_images
+        @cached_images ||= {}
+      end
+      
+      def self.dir_image
+        path = File.join(Redcar.root, %w(plugins application icons darwin-folder.png))
+        image(path)
+      end
+      
+      def self.file_image
+        path = File.join(Redcar.root, %w(plugins application icons darwin-file.png))
+        image(path)
+      end
+    end
+  end
+end
