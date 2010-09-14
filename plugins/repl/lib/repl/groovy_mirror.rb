672da5fe2ccd5327fcb7a9f4bfdea5370550dc70
delisa <iskanamagus@gmail.com>
2010-09-13 22:47:11 -0400

    GroovyREPL is lookin awesome.

--- plugins/repl/lib/repl/groovy_mirror.rb
+++ plugins/repl/lib/repl/groovy_mirror.rb
@@ -8,6 +8,8 @@ module Redcar
         unless @loaded
           require File.dirname(__FILE__) + "/../../vendor/groovy"
           import 'groovy.lang.GroovyShell'
+          import 'java.io.PrintWriter'
+          import 'java.io.StringWriter'
           @loaded = true
         end
       end
@@ -15,7 +17,7 @@ module Redcar
 
       def initialize
         GroovyMirror.load_groovy_dependencies
-        @prompt = "groovy:>"
+        @prompt = "groovy>"
         @history = "// Groovy REPL\n\n#{@prompt} "
         @instance = Main.new
       end
@@ -39,27 +41,38 @@ module Redcar
 
       class Main
         def initialize
+          @out = StringWriter.new
           @shell = GroovyShell.new
+          @shell.setProperty('out',@out)
         end
 
         def inspect
-          "groovy main"
+          "groovyREPL main"
         end
 
         def execute(cmd)
-          @shell.evaluate(cmd).to_s
+          output = @shell.evaluate(cmd).to_s
+          output = "null" unless output and not output.strip.empty?
+          if @out and not @out.toString().strip.empty?
+            console = @out.toString() + "\n"
+          else
+            console = ""
+          end
+          buf = @out.getBuffer()
+          buf.delete(0,buf.length()) if buf.length() > 0
+          console + "===> #{output}"
         end
       end
 
       def format_error(e)
         backtrace = e.backtrace
-        "ERROR: #{e.class}: #{e.message}\n        #{backtrace.join("\n        ")}"
+        "ERROR #{e.class}:\n #{e.message}\n        #{backtrace.join("\n        ")}"
       end
 
       def send_to_repl expr
         @history += expr + "\n"
         begin
-          @history += "===> " + @instance.execute(expr)
+          @history += @instance.execute(expr)
         rescue Object => e
           @history += format_error(e)
         end
