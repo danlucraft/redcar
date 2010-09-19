de7ecac5c714e9fc4a3b5cbfdd50a2691b1856b9
Daniel Lucraft <dan@fluentradical.com>
2010-09-19 11:06:42 +0100

    Redcar::Controller now has helper methods for listening to model events.

--- plugins/core/lib/core/controller.rb
+++ plugins/core/lib/core/controller.rb
@@ -1,5 +1,23 @@
 module Redcar
   module Controller
     attr_accessor :model
+    
+    def self.included(klass)
+      klass.class_eval <<-RUBY
+        def self.model_listeners
+          @model_listeners ||= []
+        end
+      
+        def self.model_listener(name)
+          model_listeners << name
+        end
+        
+        def create_model_listeners
+          self.class.model_listeners.each do |name|
+            @model.add_listener(name, &method(name))
+          end
+        end
+      RUBY
+    end
   end
 end
