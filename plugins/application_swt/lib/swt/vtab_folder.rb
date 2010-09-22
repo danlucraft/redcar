02e0628d382b47cb09e34984ff182f2a1d15e4c5
Tim Felgentreff <timfelgentreff@gmail.com>
2010-09-22 22:21:23 +0200

    import vertical tab folder work

---
+++ plugins/application_swt/lib/swt/vtab_folder.rb
@@ -0,0 +1,109 @@
+require File.expand_path("../vtab_item", __FILE__)
+
+module Swt
+  module Widgets
+    class VerticalTabFolder < Swt::Widgets::Composite
+      attr_accessor :tab_area, :content_area
+      attr_reader :selection_color_options, :font
+
+      SelectionEvent = Struct.new("Event", :item, :doit)
+
+      def initialize(parent, style)
+        super(parent, style)
+        self.layout = Swt::Layout::GridLayout.new(2, false).tap do |l|
+          l.horizontalSpacing = -1
+        end
+
+        @items = []
+        @selection_listeners = []
+        @font = Swt::Widgets::Display.current.system_font
+
+        @tab_area = Swt::Widgets::Composite.new(self, Swt::SWT::NONE).tap do |t|
+          t.layout_data = Swt::Layout::GridData.new(Swt::Layout::GridData::FILL_VERTICAL | Swt::Layout::GridData::GRAB_VERTICAL)
+          t.layout = Swt::Layout::RowLayout.new.tap do |l|
+            l.type         = Swt::SWT::VERTICAL
+            l.spacing      = -1
+            l.wrap         = false
+            l.marginLeft   = 0
+            l.marginRight  = 0
+            l.marginTop    = 0
+            l.marginBottom = 0
+          end
+        end
+      end
+
+      def set_selection_background(colors, percents, vertical = true)
+        @selection_color_options = { :colors => colors,
+          :percents => percents.collect { |i| i / 100.0 },
+          :vertical => vertical }
+      end
+
+      def add_item(tab)
+        @items << tab
+        tab.draw_label(@tab_area)
+        tab.font = @font
+        tab.active = true if @items.size == 1
+        layout
+      end
+
+      def get_item(idx)
+        return @items[idx] if idx.respond_to? :to_int
+        raise NotImplementedError, "Getting via Point not implemented"
+      end
+
+      def item_count
+        @items.size
+      end
+
+      def selection
+        @items.detect { |x| x.active? }
+      end
+
+      def selection=(tab)
+        evt = SelectionEvent.new.tap do |e|
+          e.item = tab
+          e.doit = true
+        end
+        @selection_listeners.each do |l|
+          if l.respond_to? :call
+            l[evt]
+          else
+            l.widgetSelected(evt)
+          end
+        end
+        if evt.doit
+          selection.active = false
+          if tab.respond_to? :to_int
+            @items[tab].active = true
+          else
+            tab.active = true
+          end
+          layout
+        end
+      end
+
+      def selection_index
+        index_of(selection)
+      end
+
+      def index_of(tab)
+        @items.index(tab)
+      end
+
+      def show_item(tab)
+        selection = tab
+      end
+
+      def add_selection_listener(listener = nil)
+        return @selection_listeners << listener if listener
+        raise ArgumentError, "Expected a listener or a block" unless block_given?
+        @selection_listeners << Proc.new
+      end
+
+      def font= swt_font
+        @font = swt_font
+        @items.each { |tab| tab.font = swt_font }
+      end
+    end
+  end
+end
---
+++ plugins/application_swt/lib/swt/vtab_label.rb
@@ -0,0 +1,80 @@
+require File.expand_path("../graphics_utils", __FILE__)
+
+module Swt
+  module Widgets
+    class VtabLabel
+      attr_reader :active, :title
+      attr_accessor :font
+
+      include Swt::Events::MouseListener
+
+      def initialize(tab, parent, style)
+        @label = Swt::Widgets::Label.new(parent, style)
+        @active = false
+        @tab = tab
+        @parent = parent
+        @title = ""
+
+        @label.image = label_image
+        @label.add_paint_listener { |event| event.gc.draw_image(label_image, 0, 0) }
+        @label.add_mouse_listener(self)
+      end
+
+      def label_image
+        display = Swt::Widgets::Display.current
+        @img = nil if @dirty
+        @img ||= GraphicsUtils.create_rotated_text(@title, @font, @parent.foreground, @parent.background, Swt::SWT::UP) do |gc, extent|
+          fg, bg = gc.foreground, gc.background
+          if @active
+            options = @tab.selection_color_options
+            options[:percents].each_with_index do |p, idx|
+              gc.foreground = options[:colors][idx]
+              gc.background = options[:colors][idx + 1]
+              if options[:vertical]
+                h = idx > 0 ? extent.height * options[:percents][idx - 1] : 0
+                gc.fill_gradient_rectangle(0, h, extent.width, extent.height * p, true)
+              else
+                h = idx > 0 ? extent.width * options[:percents][idx - 1] : 0
+                gc.fill_gradient_rectangle(w, 0, extent.width * p, extent.height, false)
+              end
+            end
+          else
+            gc.fill_rectangle(0, 0, extent.width, extent.height)
+          end
+          gc.foreground = display.get_system_color(Swt::SWT::COLOR_WIDGET_NORMAL_SHADOW)
+          gc.draw_rectangle(0, 0, extent.width - 1, extent.height - 1)
+          gc.foreground, gc.background = fg, bg
+        end
+        @dirty = false
+        @img
+      end
+
+      def activate
+        @tab.activate
+      end
+
+      def active= boolean
+        @active = boolean
+        redraw
+      end
+
+      def title= (str)
+        @title = str
+        redraw
+      end
+
+      def redraw
+        @dirty = true
+        @label.image = label_image
+      end
+
+      def mouseUp(e)
+        activate
+      end
+
+      # Unused
+      def mouseDown(e); end
+      def mouseDoubleClick(e); end
+    end
+  end
+end
