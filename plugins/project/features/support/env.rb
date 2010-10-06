160de0dbd9cfd5f556013036134144f53f4fb40a
delisa <iskanamagus@gmail.com>
2010-10-06 15:40:08 -0400

    Added subproject features

--- plugins/project/features/support/env.rb
+++ plugins/project/features/support/env.rb
@@ -1,14 +1,33 @@
 RequireSupportFiles File.dirname(__FILE__) + "/../../../edit_view/features/"
 
+def fixtures_path
+  File.expand_path(File.dirname(__FILE__) + "/../../spec/fixtures")
+end
+
 def reset_project_fixtures
   if @put_myproject_fixture_back
     @put_myproject_fixture_back = nil
     FileUtils.mv("plugins/project/spec/fixtures/myproject.bak",
                  "plugins/project/spec/fixtures/myproject")
   end
-  fixtures_path = File.expand_path(File.dirname(__FILE__) + "/../../spec/fixtures")
   File.open(fixtures_path + "/winter.txt", "w") {|f| f.print "Wintersmith" }
   FileUtils.rm_rf(fixtures_path + "/winter2.txt")
+  make_subproject_fixtures
+end
+
+def make_subproject_fixtures
+  FileUtils.mkdir_p(fixtures_path + "/myproject/test1")
+  FileUtils.mkdir_p(fixtures_path + "/myproject/test2")
+  FileUtils.mkdir_p(fixtures_path + "/myproject/.redcar")
+  File.open(fixtures_path + "/myproject/.redcar/test_config", "w") {|f| f.print "this is a config file" }
+  File.open(fixtures_path + "/myproject/test1/a.txt", "w") {|f| f.print "this is a project file" }
+  File.open(fixtures_path + "/myproject/test1/b.txt", "w") {|f| f.print "this is a project file" }
+  File.open(fixtures_path + "/myproject/test1/c.txt", "w") {|f| f.print "this is a project file" }
+end
+
+def delete_subproject_fixtures
+  FileUtils.rm_rf(fixtures_path + "/myproject/test1")
+  FileUtils.rm_rf(fixtures_path + "/myproject/test2")
 end
 
 Before do
@@ -17,4 +36,5 @@ end
 
 After do
   reset_project_fixtures
+  delete_subproject_fixtures
 end
