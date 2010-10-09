d43644ba310ae7dfbbe6e7a70402c056082d15eb
delisa <iskanamagus@gmail.com>
2010-10-09 00:37:35 -0400

    Updated Readme, added additional languages, fixed duplication bug

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,2 @@
-RequireSupportFiles "/boutique/Code/redcar/plugins/edit_view/features/" #TODO: fix this!
+RequireSupportFiles "/var/lib/gems/1.8/gems/redcar-0.7/plugins/edit_view/features/" #TODO: fix this! ...or merge to new branch
 require File.dirname(__FILE__) + "/../../lib/comment"
