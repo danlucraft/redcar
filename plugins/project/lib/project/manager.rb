f1240689ffe4e031273f4c443b2bef9d45dafa69
delisa <iskanamagus@gmail.com>
2010-10-13 21:41:41 -0400

    Added unicode safety

--- plugins/project/lib/project/manager.rb
+++ plugins/project/lib/project/manager.rb
@@ -145,7 +145,7 @@ module Redcar
       def self.find_projects_containing_path(path)
         open_projects.select {|p|
           p.contains_path?(path)
-        }.sort_by {|p| path.length-p.path.length}
+        }.sort_by {|p| path.split(//).length-p.path.split(//).length}
       end
 
       def self.open_file(path, adapter=Adapters::Local.new)
