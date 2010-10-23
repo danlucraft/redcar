cfeb3e6e125b1757658d67e53a5e4301433dfc1d
Tim Felgentreff <timfelgentreff@gmail.com>
2010-10-23 09:12:44 +0200

    implement abstract syntax checking classes for redcar

---
+++ lib/syntax_check/checker.rb
@@ -0,0 +1,44 @@
+module Redcar
+  module SyntaxCheck
+    class Checker
+      attr_reader :doc
+
+      def self.checkers
+        @checkers ||= {}
+      end
+
+      def self.[] name
+        checkers[name]
+      end
+
+      def self.supported_grammars(*names)
+        if names.any?
+          names.each {|g| Checker.checkers[g] = self }
+          @supported_grammars = supported_grammars + names
+        else
+          @supported_grammars ||= []
+        end
+      end
+
+      def initialize(document)
+        @doc = document
+      end
+
+      def manifest_path(doc = @doc)
+        path = doc.path
+        unless path and File.exist? path
+          raise ArgumentError, "Non-local documents not supported at this time"
+        end
+        path
+      end
+
+      def supported_grammars
+        raise NotImplementedError, "My subclass #{self.class.name} should have implemented check"
+      end
+
+      def check
+        raise NotImplementedError, "My subclass #{self.class.name} should have implemented check"
+      end
+    end
+  end
+end
