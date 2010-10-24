315d31b587a2ecc35a376f6d7b0a62433aba05c2
Tim Felgentreff <timfelgentreff@gmail.com>
2010-10-24 13:30:42 +0200

    remove explicit imports of Java classes

--- lib/syntax_check/ruby.rb
+++ lib/syntax_check/ruby.rb
@@ -4,19 +4,12 @@ require 'ruby_syntax_error'
 module Redcar
   module SyntaxCheck
     class Ruby < Checker
-
-      module Java
-        import 'java.io.File'
-        import 'java.io.FileInputStream'
-        import 'org.jruby.Ruby'
-      end
-
       supported_grammars "Ruby", "Ruby on Rails"
 
       def check(*args)
-        path = manifest_path(doc)
-        runtime = Java::Ruby.global_runtime
-        io = Java::FileInputStream.new(Java::File.new(path))
+        path    = manifest_path(doc)
+        runtime = org.jruby.Ruby.global_runtime
+        io      = java.io.FileInputStream.new(java.io.File.new(path))
         begin
           runtime.parse_from_main(io, File.basename(path))
         rescue SyntaxError => e
