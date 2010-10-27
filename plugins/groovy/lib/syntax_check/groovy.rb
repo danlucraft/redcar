eb0bd6c123611e3d033bdfbfd16a3d550d17d345
delisa <iskanamagus@gmail.com>
2010-10-27 13:01:24 -0400

    Updated to add groovy script options for setting classpath

--- plugins/groovy/lib/syntax_check/groovy.rb
+++ plugins/groovy/lib/syntax_check/groovy.rb
@@ -11,6 +11,7 @@ module Redcar
           require File.join(Redcar.asset_dir,"groovy-all")
           import 'groovy.lang.GroovyShell'
           import 'org.codehaus.groovy.control.CompilationFailedException'
+          import 'org.codehaus.groovy.control.CompilerConfiguration'
           @loaded = true
         end
       end
@@ -18,7 +19,7 @@ module Redcar
       def check(*args)
         path    = manifest_path(doc)
         name    = File.basename(path)
-        shell   = GroovyShell.new
+        shell   = create_shell
         text    = doc.get_all_text
         io      = java.io.File.new(path)
         begin
@@ -33,6 +34,36 @@ module Redcar
         line     = $1.to_i - 1
         message  = $2
         SyntaxCheck::Error.new(doc, line, message)
+      end
+
+      def classpath_files(project)
+        project.config_files("classpath.groovy")
+      end
+
+      def classpath(project)
+        parts  = []
+        shell  = GroovyShell.new
+        files  = classpath_files(project)
+        return unless files.any?
+        files.each do |path|
+          begin
+            file  = java.io.File.new(path)
+            part  = shell.run(file, [])
+          rescue Object => e
+            Redcar::Application::Dialog.message_box("Error loading groovy classpath file #{path}: #{e.message}")
+          end
+          parts = parts + part if part
+        end
+        parts
+      end
+
+      def create_shell
+        config    = CompilerConfiguration.new
+        if project = Redcar::Project::Manager.focussed_project
+          classpath = classpath(project)
+          config.setClasspathList(classpath) if classpath
+        end
+        GroovyShell.new(config)
       end
     end
   end
