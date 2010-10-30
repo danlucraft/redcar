b8762c6c30ab00073f540852123118ae5afba54a
Tim Felgentreff <timfelgentreff@gmail.com>
2010-10-30 23:33:01 +0200

    allow negation in html tab probing

--- plugins/html_view/features/step_definitions/html_view_steps.rb
+++ plugins/html_view/features/step_definitions/html_view_steps.rb
@@ -1,8 +1,8 @@
-Then /^the HTML tab (should say|says) "([^"]*)"$/ do |_, needle|
+Then /^the HTML tab (should (not )?say|says) "([^"]*)"$/ do |_, negation, needle|
   limit = 5
   contents = nil
   started = false
-  
+
   thread = Thread.new do
     started = true
     start = Time.now
@@ -10,15 +10,15 @@ Then /^the HTML tab (should say|says) "([^"]*)"$/ do |_, needle|
     while !contents or (contents and !contents.match(needle)) && Time.now - start < limit
       contents = get_browser_contents
       sleep 0.1
-    end    
+    end
   end
-  
+
   Redcar.gui.yield_until { started && !thread.alive? }
 
   # For now, just skip on XUL platforms on which we can't get browser exec results
   # (current version of SWT and XulRunner). More info at:
   # https://bugs.eclipse.org/bugs/show_bug.cgi?id=259687
   unless (contents.nil? or contents.empty?) and [:windows, :linux].include? Redcar.platform
-    contents.should match needle
+    negation ? (contents.should_not match needle) : (contents.should match needle)
   end
 end
