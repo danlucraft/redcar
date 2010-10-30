ecbe78eb1340fabe511470f0e8559d7e5eb66a73
Tim Felgentreff <timfelgentreff@gmail.com>
2010-10-31 01:27:12 +0200

    add a scenario for clicking links in the todo-list, bumping coverage close to 100% for this plugin

--- plugins/html_view/features/step_definitions/html_view_steps.rb
+++ plugins/html_view/features/step_definitions/html_view_steps.rb
@@ -22,3 +22,35 @@ Then /^the HTML tab (should (not )?say|says) "([^"]*)"$/ do |_, negation, needle
     negation ? (contents.should_not match needle) : (contents.should match needle)
   end
 end
+
+When /^I click "([^\"]+)" in the HTML tab$/ do |link|
+  html_view = focussed_tab.html_view
+  # The JQuery way doesn't seem to work - I'm getting errors with this:
+  # => html_view.controller.execute(%{ $("a:contains(#{link.gsub('"', '\"')})").click(); })
+  # So, click the link the old-fashioned way.
+  js = <<-JAVASCRIPT
+    var link, evt, links, cancelled, i;
+    links = document.getElementsByTagName("a");
+    for (i = 0; i < links.length; i++) {
+      if (links[i].innerHTML.search(#{link.inspect}) > -1) {
+        link = links[i];
+        break;
+      }
+    }
+
+    if (typeof(link) !== "undefined") {
+      if (document.createEvent) {
+        evt = document.createEvent("MouseEvents");
+        evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        cancelled = !link.dispatchEvent(evt);
+      } else if (link.fireEvent) {
+        cancelled = !link.fireEvent("onclick");
+      }
+      if (!cancelled) {
+        window.location = link.href;
+      }
+    }
+  JAVASCRIPT
+
+  html_view.controller.execute(js)
+end
--- plugins/todo_list/features/todo_list.feature
+++ plugins/todo_list/features/todo_list.feature
@@ -5,11 +5,16 @@ Feature: Adding parameters to commands
   Background:
     Given I will choose "plugins/todo_list/spec/fixtures/project" from the "open_directory" dialog
     And I open a directory
+    When I open the "Todo List" from the "Project" menu
+    And I wait "2" seconds
 
   Scenario: Opening the TODO list shows the todo items and their action texts without colons
-    When I open the "Todo List" from the "Project" menu
     Then my active tab should be "Todo List"
-    And the HTML tab should say "/project/OPTIMIZE_colon.file"
+    And the HTML tab should say "OPTIMIZE_colon.file"
     And the HTML tab should say "a course of action"
     And the HTML tab should not say ": a course of action"
 
+  Scenario: Clicking a TODO item's file name should open it in an edit tab
+    When I click "FIXME.file" in the HTML tab
+    Then my active tab should be "FIXME.file"
+    And the contents should be "#\n#\n<c># FIXME note"
