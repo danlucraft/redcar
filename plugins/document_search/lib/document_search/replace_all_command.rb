16cbfa67b722310835544096f7ce66f09d8c4e31
Tim Felgentreff <timfelgentreff@gmail.com>
2010-11-20 10:51:55 -0800

    avoid scrolling the document often

--- plugins/document_search/lib/document_search/replace_all_command.rb
+++ plugins/document_search/lib/document_search/replace_all_command.rb
@@ -1,18 +1,21 @@
 module DocumentSearch
   class ReplaceAllCommand < ReplaceCommand
     def execute
+      replacement_text, startoff = nil
       count = 0
+      sc = StringScanner.new(doc.get_all_text)
       doc.compound do
-        sc = StringScanner.new(doc.get_all_text)
         while sc.scan(query)
           count += 1
           startoff = sc.pos - sc.matched_size
           text = doc.get_range(startoff, sc.matched_size)
           replacement_text = text.gsub(query, replace)
           doc.replace(sc.pos - sc.matched_size, sc.matched_size, replacement_text)
-          doc.set_selection_range(startoff, replacement_text.length)
-          doc.scroll_to_line(doc.line_at_offset(startoff))
         end
+      end
+      if replacement_text && startoff
+        doc.set_selection_range(startoff, replacement_text.length)
+        doc.scroll_to_line(doc.line_at_offset(startoff))
       end
       count
     end
