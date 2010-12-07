d947abca69f324f21e5e313d869c687f5eb9022f
delisa <iskanamagus@gmail.com>
2010-12-07 12:24:15 -0500

    Added menu items for Syntax Checking

--- plugins/syntax_check/lib/syntax_check.rb
+++ plugins/syntax_check/lib/syntax_check.rb
@@ -7,6 +7,53 @@ module Redcar
       edit_view.remove_all_annotations :type => Error::Type
     end
 
+    def self.menus
+      Redcar::Menu::Builder.build do
+        sub_menu "Plugins" do
+          lazy_sub_menu "Syntax Checking" do
+            item "Enabled", :type => :check, :active => SyntaxCheck.enabled?, :command => ToggleSyntaxChecking
+            separator
+            Checker.checkers.each do |checker|
+              grammar = checker[0]
+              item "#{grammar}", :type => :check, :active => SyntaxCheck.check_type?(grammar), :command => ToggleGrammarChecking, :value => grammar
+            end
+          end
+        end
+      end
+    end
+
+    def self.enabled=(state)
+      SyntaxCheck.storage['suppress_syntax_checking'] = !state
+    end
+
+    def self.enabled?
+      if SyntaxCheck.storage['suppress_syntax_checking']
+        false
+      else
+        true
+      end
+    end
+
+    def self.check_type?(grammar)
+      excluded = SyntaxCheck.storage['excluded_grammars']
+      unless grammar and (excluded.include? grammar or
+        excluded.include? grammar.downcase)
+        true
+      end
+    end
+
+    def self.set_check_type(grammar,value)
+      if grammar
+        excluded = SyntaxCheck.storage['excluded_grammars']
+        if check_type?(grammar)
+          excluded << grammar.downcase
+        else
+          excluded = excluded - [grammar,grammar.downcase]
+        end
+        SyntaxCheck.storage['excluded_grammars'] = excluded
+      end
+    end
+
     def self.storage
       @storage ||= begin
         storage = Plugin::Storage.new('syntax_checking')
@@ -25,14 +72,26 @@ module Redcar
     end
 
     def self.after_save(doc)
-      excluded = SyntaxCheck.storage['excluded_grammars']
       grammar  = doc.edit_view.grammar
-      unless SyntaxCheck.storage['suppress_syntax_checking'] or
-        grammar and (excluded.include? grammar or
-        excluded.include? grammar.downcase)
-        remove_syntax_error_annotations(doc.edit_view)
+      remove_syntax_error_annotations(doc.edit_view)
+      if SyntaxCheck.enabled? and SyntaxCheck.check_type?(grammar)
         checker = Checker[doc.edit_view.grammar]
         checker.new(doc).check if checker
+      end
+    end
+
+    class ToggleGrammarChecking < Redcar::Command
+      def execute(options)
+        if grammar = options[:value]
+          value = !SyntaxCheck.check_type?(grammar)
+          SyntaxCheck.set_check_type(grammar,value)
+        end
+      end
+    end
+
+    class ToggleSyntaxChecking < Redcar::Command
+      def execute
+        SyntaxCheck.enabled = !SyntaxCheck.enabled?
       end
     end
   end
