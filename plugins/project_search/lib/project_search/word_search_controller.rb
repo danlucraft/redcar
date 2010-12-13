d8902b4e56d8295ee505d55dee99c6bd940eb904
Daniel Lucraft <dan@fluentradical.com>
2010-12-13 18:55:26 +0000

    WordSearchController no longer inherits from RegexSearchController

--- plugins/project_search/lib/project_search/word_search_controller.rb
+++ plugins/project_search/lib/project_search/word_search_controller.rb
@@ -1,7 +1,8 @@
 
 class ProjectSearch
-  class WordSearchController < ProjectSearch::RegexSearchController
-  
+  class WordSearchController
+    include Redcar::HtmlController
+
     def title
       "Project Search"
     end
@@ -18,6 +19,63 @@ class ProjectSearch
       @settings['context_lines']
     end
     
+    def index
+      @plugin_root   = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+      @settings      = ProjectSearch.storage
+      @query         = doc.selected_text if doc && doc.selection?
+      @literal_match = ProjectSearch.storage['literal_match']
+      @match_case    = ProjectSearch.storage['match_case']
+      @with_context  = ProjectSearch.storage['with_context']
+      @show_literal_match_option = show_literal_match_option?
+      @search_copy               = search_copy
+      render('index')
+    end
+
+    def edit_preferences
+      Redcar.app.make_sure_at_least_one_window_open # open a new window if needed
+      Redcar::FindInProject.storage # populate the file if it isn't already
+      tab  = Redcar.app.focussed_window.new_tab(Redcar::EditTab)
+      mirror = Project::FileMirror.new(File.join(Redcar.user_dir, "storage", "find_in_project.yaml"))
+      tab.edit_view.document.mirror = mirror
+      tab.edit_view.reset_undo
+      tab.focus
+    end
+
+    def open_file(file, line, query, literal_match, match_case)
+      Redcar::Project::Manager.open_file(File.expand_path(file, Redcar::Project::Manager.focussed_project.path))
+      doc.cursor_offset = doc.offset_at_line(line.to_i - 1)
+      regexp_text = literal_match ? Regexp.escape(query) : query
+      regex = match_case ? /(#{regexp_text})/ : /(#{regexp_text})/i
+      index = doc.get_line(line.to_i - 1).index(regex)
+      if index
+        length = doc.get_line(line.to_i - 1).match(regex)[1].length
+        doc.set_selection_range(doc.cursor_line_start_offset + index, doc.cursor_line_start_offset + index + length)
+      end
+      doc.scroll_to_line(line.to_i)
+      nil
+    end
+
+    def close
+      Thread.kill(@thread) if @thread
+      @thread = nil
+      super
+    end
+    
+    def render(view)
+      rhtml = ERB.new(File.read(File.join(File.dirname(__FILE__), "views", "#{view.to_s}.html.erb")))
+      rhtml.result(binding)
+    end
+
+    def doc
+      Redcar.app.focussed_window.focussed_notebook_tab.edit_view.document rescue false
+    end
+
+    def add_or_move_to_top(item, array)
+      return array if item.strip.empty?
+      array.delete_at(array.index(item)) if array.include?(item)
+      array.unshift(item)
+    end
+
     def search(query, literal_match, match_case, with_context)
       ProjectSearch.storage['recent_queries'] = add_or_move_to_top(query, ProjectSearch.storage['recent_queries'])
       ProjectSearch.storage['literal_match'] = (@literal_match = true)
@@ -125,6 +183,73 @@ class ProjectSearch
         end
       end
       nil
+    end
+    
+    def initialize_search_output
+      execute("$('#cached_query').val(\"#{escape_javascript(@query)}\");")
+      execute("$('#results').html(\"<div id='no_results'>Searching...</div>\");")
+      execute("$('#spinner').show();")
+      execute("$('#results_summary').hide();")
+      execute("$('#file_results_count').html(0);")
+      execute("$('#line_results_count').html(0);")
+    end
+    
+    def add_initial_table
+      # Add an initial <tr></tr>  so that tr:last can be used
+      execute("if ($('#results table').size() == 0) { $('#results').html(\"<table><tr></tr></table>\"); }")
+      execute("if ($('#results_summary').first().is(':hidden')) { $('#results_summary').show(); }")
+    end
+    
+    def increment_file_results_count
+      execute("$('#file_results_count').html(parseInt($('#file_results_count').html()) + 1);")
+    end
+    
+    def add_break_row
+      execute("$('#results table tr:last').after(\"<tr><td class='break' colspan='2'></td></tr>\");")
+    end
+    
+    def render_file_heading(name, num)
+      @file_name, @file_num = name, num
+      execute("$('#results table tr:last').after(\"#{escape_javascript(render('_file_heading'))}\");")
+    end
+    
+    def render_divider(line_file_num)
+      @line_file_num = line_file_num
+      execute("$('#results table tr:last').after(\"#{escape_javascript(render('_divider'))}\");")
+    end
+    
+    def increment_line_results_count
+      execute("$('#line_results_count').html(parseInt($('#line_results_count').html()) + 1);")
+    end
+    
+    def remove_initial_blank_tr
+      execute("$('#results table tr:first').remove();")
+    end
+
+    def render_no_results
+      result = "<div id='no_results'>No results were found using the search terms you provided.</div>"
+      execute("$('#results').html(\"#{escape_javascript(result)}\");")
+    end
+    
+    def hide_spinner
+      execute("$('#spinner').hide();")
+    end
+
+    def matching_line?(line_text)
+      line_text =~ @regexp
+    end
+
+    def escape_javascript(text)
+      escape_map = { '\\' => '\\\\', '</' => '<\/', "\r\n" => '\n', "\n" => '\n', "\r" => '\n', '"' => '\\"', "'" => "\\'" }
+      text.to_s.gsub(/(\\|<\/|\r\n|[\n\r"'])/) { escape_map[$1] }
+    end
+
+    def render_line(file_num, line_num, file_name, line_text)
+      @line_index += 1
+      @file_num, @line_num, @file_name, @line_text = file_num, line_num, file_name, line_text
+      execute("if ($('#results tr.file_#{file_num}.line_#{line_num}').size() == 0) {
+        $('#results table tr:last').after(\"#{escape_javascript(render('_file_line'))}\");
+      }")
     end
   end
 end
