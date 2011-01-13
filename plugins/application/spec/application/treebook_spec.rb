87a2109fda086dedd57dbc96fbeed9681727d9ef
delisa <iskanamagus@gmail.com>
2011-01-13 09:11:25 -0500

    [Application] Put the cursor in the tree when a tree is focussed. Two tabs move it back to the notebook.

--- plugins/application/spec/application/treebook_spec.rb
+++ plugins/application/spec/application/treebook_spec.rb
@@ -1,25 +1,25 @@
 require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 describe Redcar::Treebook do
-  
+
   describe "a new treebook" do
     before do
       @treebook = Redcar::Treebook.new
     end
-    
+
     it "has no trees" do
       @treebook.trees.length.should == 0
     end
-    
+
     it "accepts a tree and notifies listeners" do
-      tree = "mock tree"
+      tree = MockTree.new("mock tree")
       tree_added = false
       @treebook.add_listener(:tree_added) do |tree|
         tree_added = tree
       end
-      
+
       @treebook.add_tree(tree)
-      
+
       tree_added.should == tree
       @treebook.trees.should == [tree]
     end
