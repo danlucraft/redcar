9a61467bbf03662758cae22565864818eb85b153
Daniel Lucraft <dan@fluentradical.com>
2011-02-19 15:31:37 +0000

    Move Tree model into Application.

--- plugins/textmate/plugin.rb
+++ plugins/textmate/plugin.rb
@@ -4,5 +4,5 @@ Plugin.define do
   version "1.0"
   file    "lib", "textmate"
   object  "Redcar::Textmate"
-  dependencies "core", ">0", "application", ">0", "HTML View", ">=0.3.2", "tree", ">0"
+  dependencies "core", ">0", "application", ">0", "HTML View", ">=0.3.2"
 end
--- plugins/tree/plugin.rb
+++
@@ -1,9 +0,0 @@
-
-Plugin.define do
-  name    "tree"
-  version "1.0"
-  file    "lib", "tree"
-  object  "Redcar::Tree"
-  dependencies "core", ">0",
-               "application", ">0"
-end
