29cdec8abdfcfca8c0b1990f8d474a77091dd28f
Daniel Lucraft <dan@fluentradical.com>
2011-06-12 12:49:43 +0100

    Bump to 0.12.4dev

--- lib/redcar.rb
+++ lib/redcar.rb
@@ -55,7 +55,7 @@ end
 #
 # and so on.
 module Redcar
-  VERSION         = '0.12.3dev' # also change in the Rakefile!
+  VERSION         = '0.12.4dev' # also change in the gemspec!
   VERSION_MAJOR   = 0
   VERSION_MINOR   = 12
   VERSION_RELEASE = 0
