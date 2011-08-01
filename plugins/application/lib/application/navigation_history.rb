3db492e3115e1e7c1edbbf62b791a1a3f9d6499f
Shinichi Katayama <shin1.katayama@gmail.com>
2011-08-01 23:39:48 +0900

    Done rspec test and refactoring

--- plugins/application/lib/application/navigation_history.rb
+++ plugins/application/lib/application/navigation_history.rb
@@ -2,22 +2,19 @@ module Redcar
   # A class that holds a cursor navigation history. The maximum length
   # defaults to 100.
   class NavigationHistory < Array
-    def initialize()
+    def initialize
       @max_history_size = 100
       @current = 0
     end
     
-    def save(doc, move_current = true)
+    def save(doc)
       # Invoking this method means new future is about to be created, so remove old one.
       self.slice!(@current...self.size)
       
-      while self.size >= @max_history_size
-        self.delete_at(0)
-        @current -= 1
-      end
+      ensure_size_less_than_max
       
       self << {:path => doc.path, :cursor_offset => doc.cursor_offset}
-      @current += 1 if move_current
+      @current += 1
     end
     
     def can_backward?
@@ -25,7 +22,7 @@ module Redcar
     end
 
     def backward
-      change_current_and_restore(-1) if can_backward?
+      move_current_and_restore(-1) if can_backward?
     end
     
     def can_forward?
@@ -33,12 +30,32 @@ module Redcar
     end
 
     def forward
-      change_current_and_restore(1) if can_forward?
+      move_current_and_restore(1) if can_forward?
     end
     
     private
     
-    def change_current_and_restore(history_size)
+    def ensure_size_less_than_max
+      while self.size >= @max_history_size
+        self.delete_at(0)
+        @current -= 1
+      end
+    end
+    
+    def move_current_and_restore(history_size)
+      save_current_doc
+      @current += history_size
+      restore_hisotry
+    end
+    
+    def restore_hisotry
+      Project::Manager.open_file(self[@current][:path])
+      new_doc = Redcar.app.focussed_window.focussed_notebook_tab_document
+      new_doc.cursor_offset = self[@current][:cursor_offset]
+      new_doc.ensure_cursor_visible
+    end
+    
+    def save_current_doc
       current_doc = Redcar.app.focussed_window.focussed_notebook_tab_document
       if current_doc
         history = {:path => current_doc.path, :cursor_offset => current_doc.cursor_offset}
@@ -49,12 +66,6 @@ module Redcar
           @current = self.size - 1
         end
       end
-      
-      @current += history_size
-      Project::Manager.open_file(self[@current][:path])
-      new_doc = Redcar.app.focussed_window.focussed_notebook_tab_document
-      new_doc.cursor_offset = self[@current][:cursor_offset]
-      new_doc.ensure_cursor_visible
     end
   end
 end
