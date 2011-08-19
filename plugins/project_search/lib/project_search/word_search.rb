b28ff77f2672b34e66b58a50c8a91c2d2116b1aa
Delisa Mason <iskanamagus@gmail.com>
2011-08-19 01:01:57 -0400

    [Project Search] Enhanced query to regex conversion to match wildcard and operator searches
     - Changed input handler to use built-in lucene QueryParser

--- plugins/project_search/lib/project_search/word_search.rb
+++ plugins/project_search/lib/project_search/word_search.rb
@@ -1,8 +1,12 @@
 
 class ProjectSearch
   class WordSearch
+    java_import org.apache.lucene.util.Version
+    java_import org.apache.lucene.analysis.standard.StandardAnalyzer
+    java_import org.apache.lucene.queryParser.QueryParser
+
     attr_reader :query_string, :context_size, :project
-    
+
     def initialize(project, query_string, match_case, context_size)
       @project      = project
       @query_string = query_string
@@ -25,6 +29,9 @@ class ProjectSearch
     def regex
       @regex ||= begin
         regexp_text = Regexp.escape(@query_string)
+        # Replace Lucene wildcards with non-greedy Ruby regex equivalents
+        # TODO: determine best (expected?) way of handling phrases
+        regexp_text = regexp_text.gsub(/\\\s+or\\\s+/i,"|").gsub('\\*','.*?').gsub('\\?','.').gsub(/\\\s+/,".*?")
         match_case? ? /#{regexp_text}/ : /#{regexp_text}/i
       end
     end
@@ -79,24 +86,21 @@ class ProjectSearch
     def results
       @results ||= generate_results
     end
-    
-    def bits
-      query_string.
-        gsub(/[^\w]/, " ").
-        gsub("_", " ").
-        split(/\s/).
-        map {|b| b.strip}.
-        reject {|b| b == "" or org.apache.lucene.analysis.standard.StandardAnalyzer::STOP_WORDS_SET.to_a.include?(b)}
+
+    def formatted_query
+      parser = QueryParser.new(
+        Version::LUCENE_29,
+        "contents",
+        StandardAnalyzer.new(Version::LUCENE_29)
+      )
+      parser.parse(query_string).to_s
     end
-    
+
     def doc_ids
       @doc_ids ||= begin
         index = ProjectSearch.indexes[project.path].lucene_index
         doc_ids = nil
-        bits.each do |bit|
-          new_doc_ids = index.find(:contents => bit.downcase).map {|doc| doc.id }
-          doc_ids = doc_ids ? (doc_ids & new_doc_ids) : new_doc_ids
-        end
+        doc_ids = index.find(formatted_query).map {|doc| doc.id}
         doc_ids.reject {|doc_id| Redcar::Project::FileList.hide_file_path?(doc_id) }
       end
     end
