0c86c746bdc46c01cc4e9afc57b9a81254eebd1b
Daniel Lucraft <dan@fluentradical.com>
2012-01-10 21:59:37 +0000

    Make Ruby syntax checker use 1.9 syntax always, until I figure out how to make it optional

--- plugins/ruby/lib/ruby/syntax_checker.rb
+++ plugins/ruby/lib/ruby/syntax_checker.rb
@@ -1,21 +1,21 @@
 require 'java'
 
+import org.jruby.parser.Parser
+import org.jruby.parser.ParserConfiguration
+import org.jruby.CompatVersion
+
 module Redcar
   class Ruby
     class SyntaxChecker < Redcar::SyntaxCheck::Checker
       supported_grammars "Ruby", "Ruby on Rails", "RSpec"
 
       def check(*args)
-        path    = manifest_path(doc)
-        file    = File.basename(path)
-        runtime = org.jruby.Ruby.global_runtime
-        io      = java.io.FileInputStream.new(java.io.File.new(path))
+        path = manifest_path(doc)
+        file = File.basename(path)
         begin
-          runtime.parse_from_main(io, file)
+          parser.parse(file, doc.to_s.to_java.get_bytes, config_19.scope, config_19)
         rescue SyntaxError => e
           create_syntax_error(doc, e.exception.message, file).annotate
-        ensure
-          io.close
         end
       end
 
@@ -25,6 +25,26 @@ module Redcar
         message  = $2
         Redcar::SyntaxCheck::Error.new(doc, line, message)
       end
+      
+      private
+      
+      def runtime
+        org.jruby.Ruby.global_runtime
+      end
+
+      def parser
+        @parser ||= Parser.new(runtime)
+      end
+      
+      def config_19
+        @config_19 ||= ParserConfiguration.new(runtime, 0, false, CompatVersion::RUBY1_9)
+      end
+
+      def config_18
+        @config_18 ||= ParserConfiguration.new(runtime, 0, false, CompatVersion::RUBY1_8)
+      end
     end
   end
 end
+
+
